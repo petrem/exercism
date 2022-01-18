8aabca012e2d9dad5dbfe75e3e4cf3c44ba18daa
Petre Mierlutiu <petrem@users.noreply.github.com>
2022-01-18 21:32:27 +0200

    haskell:change

--- haskell/change/src/Change.hs
+++ haskell/change/src/Change.hs
@@ -1,8 +1,9 @@
 --module Change (findFewestCoins) where
 module Change where
 
+import Control.Monad (join)
 import Control.Monad.State (State, get, put, evalState)
-import qualified Data.DList as DL
+import Data.Sequence (Seq, singleton, (!?), (|>))
 import Data.Foldable (minimumBy)
 import Data.Maybe (mapMaybe)
 
@@ -14,12 +15,16 @@ type MoneyAsChange = Maybe Change
 
 
 
-{- To Do
-- replace foldl1 with foldl
-- surely there's a more idiomatic way to express foldl (>>) (map ...)
-- could ignore the state value, use execState, and use replicateM -- but then State wouldn't seem like the appropriate abstraction
-- could probably use a Reader to avoid passing the list of denominations around, revisit after I look into composing types and monad transformers?
-- a better way to keep the state then with dlist?
+{-
+
+To Do:
+- replace 'foldl1' with 'foldl'
+- surely there's a more idiomatic way to express that @foldl (>>) (map ...)@ part
+- could ignore the state value, use 'execState', and use 'replicateM' -- but then 'State' wouldn't seem like the appropriate abstraction
+- could probably use a 'Reader' to avoid passing the list of denominations around, revisit after I look into composing types and monad transformers?
+
+Changes:
+- switched from DList (dlist) to Seq in containers as Exercism doesn't find the former. Fingers crossed for the latter.
 -}
 
 findFewestCoins :: Integer -> Denominations -> MoneyAsChange
@@ -29,26 +34,30 @@ findFewestCoins target denominations
   | otherwise   = evalState (changeListUpTo denominations target) changeZero
 
 
-changeZero :: DL.DList MoneyAsChange
-changeZero = DL.singleton $ Just []
+changeZero :: Seq MoneyAsChange
+changeZero = singleton $ Just []
 
 
-changeListUpTo :: Denominations -> Integer -> State (DL.DList MoneyAsChange) MoneyAsChange
+changeListUpTo :: Denominations -> Integer -> State (Seq MoneyAsChange) MoneyAsChange
 changeListUpTo denominations upTo = foldl1 (>>) (map (addSolution denominations) [1..upTo])
 
 
-addSolution :: Denominations -> Integer -> State (DL.DList MoneyAsChange) MoneyAsChange
+addSolution :: Denominations -> Integer -> State (Seq MoneyAsChange) MoneyAsChange
 addSolution denominations target = do
   solutions <- get
   let usableCoins = filter (<= target) denominations
       changeVariants =
-        mapMaybe (\c -> (c:) <$> (DL.toList solutions !! fromIntegral (target-c))) usableCoins
+        -- solutions !? target -c :: MoneyAsChange ~ Maybe [Integer]
+        mapMaybe (\c -> (c:) <$> join (solutions !? fromIntegral (target-c))) usableCoins
       change = if null changeVariants
                then Nothing
                else Just $ minimumBy compareByLength changeVariants
-  put $ DL.snoc solutions change
+  put $ solutions |> change
   return change
 
+
+(<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
+(<$$>) = fmap . fmap
 
 compareByLength :: (Ord a, Foldable t) => t a -> t a -> Ordering
 compareByLength a b = compare (length a) (length b)
