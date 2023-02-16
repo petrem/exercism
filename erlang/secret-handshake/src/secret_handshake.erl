ac0746c77730c917bca3b688a1d730f3983af9ed
Petre Mierlutiu <petrem@users.noreply.gitlab.com>
2023-02-16 15:37:04 +0200

    exercism:erlang:secret-handshake

--- erlang/secret-handshake/src/secret_handshake.erl
+++ erlang/secret-handshake/src/secret_handshake.erl
@@ -3,25 +3,18 @@
 -export([commands/1]).
 
 commands(_Number) ->
-    Bits = [2#00001, 2#00010, 2#00100, 2#01000, 2#10000],
-    commands(Bits, _Number, []).
+    RegularActions = [ {2#00001, "wink"}
+                     , {2#00010, "double blink"}
+                     , {2#00100, "close your eyes"}
+                     , {2#01000, "jump"}],
+    Reverse = 2#10000,
+    PerformedActions = [_Action || {_Bit, _Action} <- RegularActions, has_bit(_Number, _Bit)],
+    case has_bit(_Number, Reverse) of
+       false ->
+            PerformedActions;
+       true ->
+            lists:reverse(PerformedActions)
+    end.
 
-commands([], _, _Acc) ->
-    lists:reverse(_Acc);
-commands([_Bit | _Rest], _Number, _Acc) when _Bit band _Number /= 0 ->
-    commands(_Rest
-            , _Number
-            , case _Bit of
-                  2#00001 -> ["wink" | _Acc];
-                  2#00010 -> ["double blink" | _Acc];
-                  2#00100 -> ["close your eyes" | _Acc];
-                  2#01000 -> ["jump" | _Acc];
-                  2#10000 -> lists:reverse(_Acc)
-              end
-            );
-commands([_Bit | _Rest], _Number, _Acc) when _Bit band _Number == 0 ->
-    commands(_Rest, _Number, _Acc).
-                                
-           
-                                      
-            
+has_bit(_Number, _Bit) ->
+    _Number band _Bit /= 0.
