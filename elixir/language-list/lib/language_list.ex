813af642782809bfea1da580e1123b88033c5165
Petre Mierlutiu <petrem@archeryevent.ro>
2023-02-25 18:23:37 +0200

    exercism:elixir:language-list

---
+++ elixir/language-list/lib/language_list.ex
@@ -0,0 +1,22 @@
+defmodule LanguageList do
+  def new(), do: []
+
+  def add(list, language), do: [language | list]
+
+  def remove(list) do
+    [_ | tail] = list
+    tail
+  end
+
+  def first(list) do
+    hd list
+  end
+
+  def count(list) do
+    length(list)
+  end
+
+  def functional_list?(list) do
+    "Elixir" in list
+  end
+end
