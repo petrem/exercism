4316647cd1c06545ee310c6b4dcdb01c9b298b49
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-03-27 15:55:00 +0300

    exercism:elixir:binary

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -2,4 +2,19 @@
 #include "binary.h"
 
 int convert(const char *input) {
+  int acc = 0;
+  while (*input && *(input) == '0') input++;
+  for (;*input; input++) {
+    acc <<= 1;
+    switch (*input) {
+    case '1':
+      acc |= 1;
+      break;
+    case '0':
+      break;
+    default:
+      return INVALID;
+    }
+  }
+  return acc;
 }
