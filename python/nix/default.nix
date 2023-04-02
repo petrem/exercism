ecabf76a7a22fa808029a8afd0291a658d2ac0c3
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-04-02 16:01:12 +0300

    exercism: python: nix boilerplate

---
+++ python/nix/default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {}
+, python ? pkgs.python39
+, pythonPackages ? pkgs.python39Packages
+, extra_python_packages ? []
+}:
+
+let
+  app_python_packages = with pythonPackages; [];
+  python_env = (
+      python.buildEnv.override  {
+        extraLibs = builtins.concatLists [ app_python_packages extra_python_packages];
+      }
+    );
+in pkgs.buildEnv {
+  name = "exercism-python-environment";
+  paths = [
+    pkgs.jq  # we don't really need jq, but maybe we'd need something else
+    python_env
+  ];
+}
---
+++ python/shell.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import <nixpkgs> {config.allowUnfree = true;} }:
+
+with pkgs;
+let
+  inherit (lib) optional optionals;
+  python = pkgs.python39;
+  pythonPackages = pkgs.python39Packages;
+in
+
+mkShell {
+  buildInputs = [
+    (
+      import ./nix/default.nix { inherit pkgs;
+                                 inherit python;
+                                 inherit pythonPackages;
+                                 extra_python_packages = with pythonPackages; [
+                                   pip      # to explore around...
+                                   ipython  # repl is good
+                                   # running tests
+                                   pytest
+                                   pytest-cache
+                                   pytest-subtests
+                                   pytest-pylint
+                                   # linting
+                                   # todo: how to specify prospector[with_vulture, with_mypy, with_bandit] ?
+                                   prospector
+                                   vulture
+                                   mypy
+                                   bandit
+                                   # sometimes used while debugging
+                                   rich
+                                   icecream
+                                 ];
+                               }
+    )
+    direnv
+    entr
+    exercism
+    git
+    just
+  ] ++ optional stdenv.isLinux inotify-tools
+  ++ optional stdenv.isDarwin terminal-notifier
+  ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    CoreFoundation
+    CoreServices
+  ]);
+}
