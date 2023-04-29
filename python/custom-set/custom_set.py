b5633c3e51bb4e249557c334f0a7525a0a4b7d5d
Petre Mierlutiu <petrem@archeryevent.ro>
2023-04-29 16:16:24 +0300

    python: custom-set: add chaining a proper(?) hashing

--- python/custom-set/custom_set.py
+++ python/custom-set/custom_set.py
@@ -1,25 +1,36 @@
-"""An erroneous solution that passes the tests.
+"""Custom Sets: or is unit testing pointless?
 
-The most evident issue is that the hash table implementation does not handle collisions.
+(No, it is not, but we have to be careful)
 
-A small number of tests re-written using hypothesis exposes the problem quickly: given
+
+Iternation 1: An erroneous solution that passes the tests. The most evident issue is
+that the hash table implementation does not handle collisions.
+
+A small number of tests re-written using `hypothesis` exposes the problem quickly: given
 we use 7 as the hashtable size and as the prime for the hash function (division-based):
 
 ```
 FAILED custom_set_prop_test.py::test_when_the_element_is_not_in_the_set - assert (7 not in CustomSet([0])) is True
 ```
 
-See `custom_set_prop_test.py`. Assumes you have pytest and hypothesis installed.
+See `custom_set_prop_test.py`. Assumes you have `pytest` and `hypothesis` installed.
+
+Iteration 2: Adding chaining solves the exposed issues. Changed the hash function to a
+multiplicaton-based one and use `hash()` instead of `id()` which of course caused issues
+for equal, but distinct objects. There are other various minor updates, and more
+hypothesis-based testing. If you suspect I'm fairly new at using `hypothesis`, you are
+correct.
 
 """
-
-from itertools import chain
+import math
+import sys
+from copy import deepcopy
 
 
 class CustomSet:
     _HASHTABLE_SIZE = 7
 
-    def __init__(self, elements=[]):
+    def __init__(self, elements=()):
         self._hashtable = _HashTable(CustomSet._HASHTABLE_SIZE)
         for element in elements:
             self.add(element)
@@ -37,25 +48,30 @@ class CustomSet:
         return self.intersection(other).isempty()
 
     def __eq__(self, other):
-        return len(self) == len(other) and all(
-            e in self for e in other
-        )
+        return len(self) == len(other) and all(e in self for e in other)
 
     def add(self, element):
         self._hashtable.add(element)
 
     def intersection(self, other):
-        return CustomSet(
-            e for e in other if e in self
-        )
+        return CustomSet(e for e in other if e in self)
 
     def __sub__(self, other):
         return CustomSet(e for e in self if e not in other)
 
     def __add__(self, other):
-        return CustomSet(chain(self, other))
+        larger, smaller = (self, other) if len(self) >= len(other) else (other, self)
+        union = larger.copy()
+        for element in smaller:
+            union.add(element)
+        return union
 
-    # extra-curricular dunders -- consider them private
+    # Extra-curricular methods -- consider them private (e.g. for tests purposes).
+
+    def copy(self):
+        obj = super().__new__(CustomSet)
+        obj._hashtable = self._hashtable.copy()
+        return obj
 
     def __iter__(self):
         return iter(self._hashtable)
@@ -64,49 +80,88 @@ class CustomSet:
         return len(self._hashtable)
 
     def __repr__(self):
-        elements = ",".join(str(e) for e in self._hashtable)
+        elements = ",".join(repr(e) for e in self._hashtable)
         return f"CustomSet([{elements}])"
 
 
-class _HashTable:
-    NIL = object()
+WORD_WIDTH = sys.hash_info.width
+NEXT_POWER_OF_TWO = 2 ** (WORD_WIDTH + 1)
 
-    def __init__(self, size: int):
-        self.size = size
-        self._table = [_HashTable.NIL] * size
+
+class _HashTable:
+    s_Factor = int(2 ** (WORD_WIDTH - 1) * (math.sqrt(5) - 1))
+
+    def __init__(self, size_exp: int):
+        if not 0 < size_exp <= WORD_WIDTH:
+            raise ValueError(
+                f"Hash size exponent {size_exp} must be between 1 and {WORD_WIDTH}"
+            )
+        self.hash_bits_displacement = WORD_WIDTH - size_exp
+        self.hash_mask = (2**size_exp - 1) << self.hash_bits_displacement
+        self.size = 2**size_exp
+        self.len = 0
+        self._table = [[] for _ in range(self.size)]
 
     def _hash(self, obj):
-        return id(obj) % self.size
+        k = hash(obj)
+        if k < 0:
+            k = NEXT_POWER_OF_TWO - k
+        return (
+            (k * _HashTable.s_Factor) & self.hash_mask
+        ) >> self.hash_bits_displacement
 
     def __len__(self):
-        return sum(1 for e in self._table if e is not _HashTable.NIL)
+        return self.len
 
     def __contains__(self, element):
-        return self._table[self._hash(element)] is not _HashTable.NIL
+        return element in self._table[self._hash(element)]
 
     def __iter__(self):
         return _HashTableIterator(self._table)
 
     def add(self, element):
-        self._table[self._hash(element)] = element
+        hv = self._hash(element)
+        if element not in self._table[hv]:
+            self._table[hv].append(element)
+            self.len += 1
+
+    def extend(self, elements):
+        """Add several elements at once.
+
+        Maybe we could make it more efficient then adding them one by one?.
+        """
+        for element in elements:
+            self.add(element)
+
+    def copy(self):
+        clone = super().__new__(_HashTable)
+        clone.size = self.size
+        clone.len = self.len
+        clone.hash_mask = self.hash_mask
+        clone.hash_bits_displacement = self.hash_bits_displacement
+        clone._table = deepcopy(self._table)
+        return clone
 
     def __repr__(self):
-        elements = ",".join(str(e) for e in self._table if e is not _HashTable.NIL)
+        elements = ",".join(repr(e) for e in self)
         return f"_HashTable([{elements}])"
 
 
 class _HashTableIterator:
     def __init__(self, table):
         self._table = table
-        self._current = -1
+        self._current_slot = 0
+        self._current_in_chain = 0
 
     def __next__(self):
-        self._current += 1
-        while self._current < len(self._table):
-            element = self._table[self._current]
-            if element is not _HashTable.NIL:
+        while self._current_slot < len(self._table):
+            slot_chain = self._table[self._current_slot]
+            if self._current_in_chain < len(slot_chain):
+                element = slot_chain[self._current_in_chain]
+                self._current_in_chain += 1
                 return element
-            self._current += 1
+            self._current_in_chain = 0
+            self._current_slot += 1
         raise StopIteration
 
     def __iter__(self):
--- python/custom-set/custom_set_prop_test.py
+++ python/custom-set/custom_set_prop_test.py
@@ -1,40 +1,65 @@
-from hypothesis import example, given, strategies as st
-
 from custom_set import CustomSet
-
+from hypothesis import example, given, note
+from hypothesis import strategies as st
 
 # Strategies
 
+ELEMENTS = (st.integers(), st.text(), st.dates())
+SMALL_INTS = st.integers(min_value=0, max_value=100)
+
+
 @st.composite
-def disjoint_lists(draw, elements=st.integers(min_value=0, max_value=100)):
-    xs = draw(st.lists(elements, min_size=1))
-    ys = draw(st.lists(elements, min_size=1).filter(lambda ys: set(ys).isdisjoint(xs)))
+def list_of_things(draw, min_size=0, elements=ELEMENTS):
+    return draw(st.lists(st.one_of(elements), min_size=min_size))
+
+
+def nonempty_list_of_things():
+    return list_of_things(min_size=1)
+
+
+def nonempty_list_of_small_ints():
+    return list_of_things(min_size=1, elements=(SMALL_INTS,))
+
+
+def nonempty_list_of_ints():
+    return list_of_things(min_size=1, elements=(st.integers(),))
+
+
+@st.composite
+def disjoint_lists_of_unique_elements(draw, elements=SMALL_INTS, min_size=1):
+    xs = draw(st.lists(elements, min_size=min_size, unique=True))
+    ys = draw(
+        st.lists(elements, min_size=min_size, unique=True).filter(
+            lambda ys: set(ys).isdisjoint(xs)
+        )
+    )
     return xs, ys
 
 
 # TESTS
 # Copied the tests from custom_set_test.py, and adapt them for property testing
 
+
 def test_sets_with_no_elements_are_empty():
     sut = CustomSet()
     assert sut.isempty() is True
 
 
-@given(st.lists(st.one_of(st.integers(), st.text(), st.dates()), min_size=1))
+@given(nonempty_list_of_things())
 def test_sets_with_elements_are_not_empty(xs):
     sut = CustomSet(xs)
     assert sut.isempty() is False
 
 
-@given(st.integers(min_value=0, max_value=200))
+@given(st.integers(min_value=0, max_value=100))
 def test_nothing_is_contained_in_an_empty_set(x):
-    # not sure what this is supposed to achieve; the origin test
+    # not sure what this is supposed to achieve; the original test
     # checks that `1` is not in the (empty) set.
     sut = CustomSet()
     assert (x in sut) is False
 
 
-@given(st.lists(st.integers(min_value=0, max_value=100), min_size=1))
+@given(nonempty_list_of_small_ints())
 @example([1, 2, 2])  # make sure we have a duplicate
 def test_when_the_element_is_in_the_set(xs):
     sut = CustomSet(xs)
@@ -42,194 +67,264 @@ def test_when_the_element_is_in_the_set(xs):
         assert x in sut
 
 
-@given(disjoint_lists())
-def test_when_the_element_is_not_in_the_set(xys):
-    xs, ys = xys
+@given(disjoint_lists_of_unique_elements())
+def test_when_the_element_is_not_in_the_set(xs_ys):
+    xs, ys = xs_ys
     sut = CustomSet(xs)
     for y in ys:
         assert (y not in sut) is True
 
-# def test_empty_set_is_a_subset_of_another_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     .assertIs(set1.issubset(set2), True)
 
-# def test_empty_set_is_a_subset_of_non_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([1])
-#     .assertIs(set1.issubset(set2), True)
+@given(nonempty_list_of_things())
+@example([])
+def test_empty_set_is_a_subset_of_another_set(xs):
+    # Joined two of the original tests into one (empty set is subset of empty/non-empty)
+    set1 = CustomSet()
+    set2 = CustomSet(xs)
+    assert set1.issubset(set2) is True
 
-# def test_non_empty_set_is_not_a_subset_of_empty_set():
-#     set1 = CustomSet([1])
-#     set2 = CustomSet()
-#     .assertIs(set1.issubset(set2), False)
 
-# def test_set_is_a_subset_of_set_with_exact_same_elements():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([1, 2, 3])
-#     .assertIs(set1.issubset(set2), True)
+@given(nonempty_list_of_things())
+def test_non_empty_set_is_not_a_subset_of_empty_set(xs):
+    set1 = CustomSet(xs)
+    set2 = CustomSet()
+    assert set1.issubset(set2) is False
 
-# def test_set_is_a_subset_of_larger_set_with_same_elements():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([4, 1, 2, 3])
-#     .assertIs(set1.issubset(set2), True)
 
-# def test_set_is_not_a_subset_of_set_that_does_not_contain_its_elements():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([4, 1, 3])
-#     .assertIs(set1.issubset(set2), False)
+@given(nonempty_list_of_things())
+def test_set_is_a_subset_of_set_with_exact_same_elements(xs):
+    set1 = CustomSet(xs)
+    set2 = CustomSet(xs)
+    assert set1.issubset(set2) is True
 
-# def test_the_empty_set_is_disjoint_with_it():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     .assertIs(set1.isdisjoint(set2), True)
 
-# def test_empty_set_is_disjoint_with_non_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([1])
-#     .assertIs(set1.isdisjoint(set2), True)
+@given(disjoint_lists_of_unique_elements())
+def test_set_is_a_subset_of_larger_set_with_same_elements(xs_ys):
+    xs, ys = xs_ys
+    set1 = CustomSet(xs)
+    set2 = CustomSet(xs + ys)
+    assert set1.issubset(set2) is True
 
-# def test_non_empty_set_is_disjoint_with_empty_set():
-#     set1 = CustomSet([1])
-#     set2 = CustomSet()
-#     .assertIs(set1.isdisjoint(set2), True)
 
-# def test_sets_are_not_disjoint_if_they_share_an_element():
-#     set1 = CustomSet([1, 2])
-#     set2 = CustomSet([2, 3])
-#     .assertIs(set1.isdisjoint(set2), False)
+@given(disjoint_lists_of_unique_elements(min_size=2))
+def test_set_is_not_a_subset_of_set_that_does_not_contain_its_elements(xs_ys):
+    xs, ys = xs_ys
+    # pick some elements from ys
+    ys_left, ys_right = ys[len(ys) // 2 :], ys[: len(ys) // 2]
+    set1 = CustomSet(xs + ys_left)
+    set2 = CustomSet(xs + ys_right)
+    assert set1.issubset(set2) is False
 
-# def test_sets_are_disjoint_if_they_share_no_elements():
-#     set1 = CustomSet([1, 2])
-#     set2 = CustomSet([3, 4])
-#     .assertIs(set1.isdisjoint(set2), True)
 
-# def test_empty_sets_are_equal():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     .assertEqual(set1, set2)
+@given(list_of_things())
+@example([])
+def test_the_empty_set_is_disjoint_with_any_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    assert empty_set.isdisjoint(other_set) is True
 
-# def test_empty_set_is_not_equal_to_non_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([1, 2, 3])
-#     .assertNotEqual(set1, set2)
 
-# def test_non_empty_set_is_not_equal_to_empty_set():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet()
-#     .assertNotEqual(set1, set2)
+@given(list_of_things())
+@example([])
+def test_any_setis_dijoint_with_the_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    assert other_set.isdisjoint(empty_set) is True
 
-# def test_sets_with_the_same_elements_are_equal():
-#     set1 = CustomSet([1, 2])
-#     set2 = CustomSet([2, 1])
-#     .assertEqual(set1, set2)
 
-# def test_sets_with_different_elements_are_not_equal():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([1, 2, 4])
-#     .assertNotEqual(set1, set2)
+@given(disjoint_lists_of_unique_elements(), st.integers(min_value=0, max_value=100))
+def test_sets_are_not_disjoint_if_they_share_an_element(xs_ys, z):
+    xs, ys = xs_ys
+    set1 = CustomSet(xs + [z])
+    set2 = CustomSet(ys + [z])
+    assert set1.isdisjoint(set2) is False
 
-# def test_set_is_not_equal_to_larger_set_with_same_elements():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([1, 2, 3, 4])
-#     .assertNotEqual(set1, set2)
 
-# def test_add_to_empty_set():
-#     sut = CustomSet()
-#     expected = CustomSet([3])
-#     sut.add(3)
-#     .assertEqual(sut, expected)
+@given(disjoint_lists_of_unique_elements(min_size=0))
+def test_sets_are_disjoint_if_they_share_no_elements(xs_ys):
+    xs, ys = xs_ys
+    set1 = CustomSet(xs)
+    set2 = CustomSet(ys)
+    assert set1.isdisjoint(set2) is True
 
-# def test_add_to_non_empty_set():
-#     sut = CustomSet([1, 2, 4])
-#     expected = CustomSet([1, 2, 3, 4])
-#     sut.add(3)
-#     .assertEqual(sut, expected)
 
-# def test_adding_an_existing_element_does_not_change_the_set():
-#     sut = CustomSet([1, 2, 3])
-#     expected = CustomSet([1, 2, 3])
-#     sut.add(3)
-#     .assertEqual(sut, expected)
+def test_empty_sets_are_equal():
+    # explicitly check empty set equality, because
+    # `test_sets_with_elements_are_not_empty` below uses `st.data` which is
+    # not compatible with `@example`.
+    set1 = CustomSet()
+    set2 = CustomSet()
+    assert set1 == set2
 
-# def test_intersection_of_two_empty_sets_is_an_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     expected = CustomSet()
-#     .assertEqual(set1.intersection(set2), expected)
 
-# def test_intersection_of_an_empty_set_and_non_empty_set_is_an_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([3, 2, 5])
-#     expected = CustomSet()
-#     .assertEqual(set1.intersection(set2), expected)
+@given(nonempty_list_of_things())
+def test_empty_set_is_not_equal_to_non_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    assert empty_set != other_set
 
-# def test_intersection_of_a_non_empty_set_and_an_empty_set_is_an_empty_set():
-#     set1 = CustomSet([1, 2, 3, 4])
-#     set2 = CustomSet()
-#     expected = CustomSet()
-#     .assertEqual(set1.intersection(set2), expected)
 
-# def test_intersection_of_two_sets_with_no_shared_elements_is_an_empty_set():
-#     set1 = CustomSet([1, 2, 3])
-#     set2 = CustomSet([4, 5, 6])
-#     expected = CustomSet()
-#     .assertEqual(set1.intersection(set2), expected)
+@given(nonempty_list_of_things())
+def test_non_empty_set_is_not_equal_to_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    assert other_set != empty_set
 
-# def test_intersection_of_two_sets_with_shared_elements_is_a_set_of_the_shared_elements(
-#     ,
-# ):
-#     set1 = CustomSet([1, 2, 3, 4])
-#     set2 = CustomSet([3, 2, 5])
-#     expected = CustomSet([2, 3])
-#     .assertEqual(set1.intersection(set2), expected)
 
-# def test_difference_of_two_empty_sets_is_an_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     expected = CustomSet()
-#     .assertEqual(set1 - set2, expected)
+@given(list_of_things(), st.data())
+def test_sets_with_the_same_elements_are_equal(xs, data):
+    set1 = CustomSet(xs)
+    set2 = CustomSet(data.draw(st.permutations(xs)))
+    assert set1 == set2
 
-# def test_difference_of_empty_set_and_non_empty_set_is_an_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([3, 2, 5])
-#     expected = CustomSet()
-#     .assertEqual(set1 - set2, expected)
 
-# def test_difference_of_a_non_empty_set_and_an_empty_set_is_the_non_empty_set():
-#     set1 = CustomSet([1, 2, 3, 4])
-#     set2 = CustomSet()
-#     expected = CustomSet([1, 2, 3, 4])
-#     .assertEqual(set1 - set2, expected)
+@given(disjoint_lists_of_unique_elements())
+def test_sets_with_different_elements_are_not_equal(xs_ys):
+    xs, ys = xs_ys
+    # pick some elements from ys
+    ys_left, ys_right = ys[len(ys) // 2 :], ys[: len(ys) // 2]
+    set1 = CustomSet(xs + ys_left)
+    set2 = CustomSet(xs + ys_right)
+    assert set1 != set2
 
-# def test_difference_of_two_non_empty_sets_is_a_set_of_elements_that_are_only_in_the_first_set(
-#     ,
-# ):
-#     set1 = CustomSet([3, 2, 1])
-#     set2 = CustomSet([2, 4])
-#     expected = CustomSet([1, 3])
-#     .assertEqual(set1 - set2, expected)
 
-# def test_union_of_empty_sets_is_an_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet()
-#     expected = CustomSet()
-#     .assertEqual(set1 + set2, expected)
+@given(disjoint_lists_of_unique_elements())
+def test_set_is_not_equal_to_larger_set_with_same_elements(xs_ys):
+    xs, ys = xs_ys
+    set1 = CustomSet(xs)
+    set2 = CustomSet(xs + ys)
+    assert set1 != set2
 
-# def test_union_of_an_empty_set_and_non_empty_set_is_the_non_empty_set():
-#     set1 = CustomSet()
-#     set2 = CustomSet([2])
-#     expected = CustomSet([2])
-#     .assertEqual(set1 + set2, expected)
 
-# def test_union_of_a_non_empty_set_and_empty_set_is_the_non_empty_set():
-#     set1 = CustomSet([1, 3])
-#     set2 = CustomSet()
-#     expected = CustomSet([1, 3])
-#     .assertEqual(set1 + set2, expected)
+@given(st.one_of(ELEMENTS))
+def test_add_to_empty_set(x):
+    sut = CustomSet()
+    expected = CustomSet([x])
+    sut.add(x)
+    assert sut == expected
 
-# def test_union_of_non_empty_sets_contains_all_unique_elements():
-#     set1 = CustomSet([1, 3])
-#     set2 = CustomSet([2, 3])
-#     expected = CustomSet([3, 2, 1])
-#     .assertEqual(set1 + set2, expected)
+
+@given(nonempty_list_of_things(), st.one_of(ELEMENTS))
+def test_add_to_non_empty_set(xs, x):
+    sut = CustomSet(xs)
+    expected = CustomSet(xs + [x])
+    sut.add(x)
+    assert sut == expected
+
+
+@given(nonempty_list_of_things(), st.data())
+def test_adding_an_existing_element_does_not_change_the_set(xs, data):
+    sut = CustomSet(xs)
+    x = data.draw(st.sampled_from(xs))
+    expected = CustomSet(xs)
+    sut.add(x)
+    assert sut == expected
+
+
+@given(list_of_things())
+@example([])
+def test_intersection_of_an_empty_set_and_another_set_is_an_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    expected = CustomSet()
+    assert empty_set.intersection(other_set) == expected
+
+
+@given(list_of_things())
+@example([])
+def test_intersection_of_a_set_and_an_empty_set_is_an_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    expected = CustomSet()
+    assert other_set.intersection(empty_set) == expected
+
+
+@given(disjoint_lists_of_unique_elements())
+def test_intersection_of_two_sets_with_no_shared_elements_is_an_empty_set(xs_ys):
+    xs, ys = xs_ys
+    set1 = CustomSet(xs)
+    set2 = CustomSet(ys)
+    expected = CustomSet()
+    assert set1.intersection(set2) == expected
+
+
+@given(disjoint_lists_of_unique_elements())
+def test_intersection_of_two_sets_with_shared_elements_is_a_set_of_the_shared_elements(
+    xs_ys,
+):
+    xs, ys = xs_ys
+    # pick some elements from ys
+    ys_left, ys_right = ys[len(ys) // 2 :], ys[: len(ys) // 2]
+    set1 = CustomSet(xs + ys_left)
+    set2 = CustomSet(xs + ys_right)
+    expected = CustomSet(xs)
+    assert set1.intersection(set2) == expected
+
+
+@given(list_of_things())
+@example([])
+def test_difference_of_empty_set_and_another_set_is_an_empty_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    expected = CustomSet()
+    assert empty_set - other_set == expected
+
+
+@given(list_of_things())
+@example([])
+def test_difference_of_a_set_and_an_empty_set_is_the_set(xs):
+    empty_set = CustomSet()
+    other_set = CustomSet(xs)
+    expected = CustomSet(xs)
+    assert other_set - empty_set == expected
+
+
+@given(disjoint_lists_of_unique_elements(min_size=2))
+def test_difference_of_two_non_empty_sets_is_a_set_of_elements_that_are_only_in_the_first_set(
+    xs_ys,
+):
+    xs, ys = xs_ys
+    # pick some elements from ys
+    ys_left, ys_right = ys[len(ys) // 2 :], ys[: len(ys) // 2]
+    set1 = CustomSet(xs + ys_left)
+    note(f"Set1: {set1!r}")
+    set2 = CustomSet(xs + ys_right)
+    note(f"Set2: {set2!r}")
+    expected = CustomSet(ys_left)
+    note(f"Expected: {expected!r}")
+    assert set1 - set2 == expected
+
+
+@given(nonempty_list_of_things())
+def test_union_of_an_empty_set_and_other_is_other(xs):
+    set1 = CustomSet()
+    set2 = CustomSet(xs)
+    expected = CustomSet(xs)
+    assert set1 + set2 == expected
+
+
+@given(nonempty_list_of_things())
+def test_union_of_some_set_and_an_empty_set_is_first_set(xs):
+    set1 = CustomSet(xs)
+    set2 = CustomSet()
+    expected = CustomSet(xs)
+    assert set1 + set2 == expected
+
+
+@given(nonempty_list_of_things(), nonempty_list_of_things())
+def test_union_is_commutative(xs, ys):
+    set1 = CustomSet(xs)
+    set2 = CustomSet(ys)
+    u1 = set1 + set2
+    u2 = set2 + set1
+    note(f"Union1: {u1!r} table: {u1._hashtable._table}")
+    note(f"Union2: {u2!r} table: {u2._hashtable._table}")
+    assert set1 + set2 == set2 + set1
+
+
+@given(nonempty_list_of_small_ints(), nonempty_list_of_small_ints())
+def test_union_of_non_empty_sets_contains_all_unique_elements(xs, ys):
+    set1 = CustomSet(xs)
+    set2 = CustomSet(ys)
+    expected = CustomSet(set(xs + ys))
+    assert set1 + set2 == expected
