25d2ef921230b637651e0085f711dd854cc8a42d
Petre Mierlutiu <petrem@archeryevent.ro>
2023-05-21 13:14:34 +0300

    exercism:elixir:isogram

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -1,1 +1,11 @@
-isogram(Phrase).
+isogram(Phrase):-
+    string_codes(Phrase, Codes), isogram_helper(Codes, []).
+
+isogram_helper([], _):- true.
+isogram_helper([H|T], Chars):-
+    not(char_type(H, alpha)),
+    isogram_helper(T, Chars).
+isogram_helper([H|T], Chars):-
+    char_type(H, to_upper(LowerH)),
+    not(member(LowerH, Chars)),
+    isogram_helper(T, [LowerH|Chars]).
