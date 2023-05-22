ea3a2e49162f7d6ba22b1e2b4b1ee9cd10a541be
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-05-22 19:55:31 +0300

    exercism:elixir:raindrops

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -1,2 +1,20 @@
-convert(N, "Pling"):-
-    0 is mod(N, 3).
+convert(N, Str):-
+    raindrops(N, [(3, "Pling"), (5, "Plang"), (7, "Plong")], Raindrops),
+    [_|_] = Raindrops,
+    concat_string_list(Raindrops, Str),
+    !.
+convert(N, Str):- number_string(N, Str).
+
+raindrops(_, [], []):- !.
+raindrops(N, [(X, Drop)|T], [Drop|Rs]):-
+    (0 is mod(N, X)),
+    raindrops(N, T, Rs),
+    !.
+raindrops(N, [_|T], Rs):-
+    raindrops(N, T, Rs), !.
+
+concat_string_list([], "").
+concat_string_list([H|T], Rez):-
+    concat_string_list(T, S),
+    string_concat(H, S, Rez).
+
