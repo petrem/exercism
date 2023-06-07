65ba62b8d0601afed13fe394ab3241dbbeb73c79
Petre Mierlutiu <petrem@archeryevent.ro>
2023-06-07 20:24:27 +0300

    exercism:haskell:leap use logical expression with an applicative twist

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,9 +1,17 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year
-  | not $ isDivisibleBy 4 = False
-  | isDivisibleBy 100 = isDivisibleBy 400
-  | otherwise = True
-    where isDivisibleBy = (== 0) . (year `rem`)
+isLeapYear = (isDivisibleBy 4) <&&> (isDivisibleBy 100 <->> isDivisibleBy 400)
 
+isDivisibleBy :: Integral a => a -> a -> Bool
+isDivisibleBy x y = (y `rem` x) == 0
+
+-- | "Applicative" Implication
+infixr 4 <->>
+(<->>) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+p1 <->> p2 = (||) <$> not . p1 <*> p2
+
+-- | "Applicative" Conjunction
+infixr 3 <&&>
+(<&&>) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+p1 <&&> p2 = (&&) <$> p1 <*> p2
