59e5b7ae92a243273f4c0a6a72255ae828b537ad
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-06-12 16:15:55 +0300

    exercism:emacs-lisp:matching-brackets

--- emacs-lisp/matching-brackets/matching-brackets.el
+++ emacs-lisp/matching-brackets/matching-brackets.el
@@ -3,48 +3,46 @@
 ;;; Commentary:
 
 ;; This version employs dolist instead of recursion.
-;; I continue to be pretty sure there should be a built-in for
-;; filter, at least.
+;; I'm using the prefix minus (e.g. `-foo-bar `) to mark things "private" to the module,
+;; but I'm almost certainly wrong to do so.
 
 ;;; Code:
 
 
 (defun is-paired (value)
   "Check if VALUE has properly matched round, square and curly parens."
-  (pair-helper-p
-   (filter-dolist (lambda (x) (member x '("(" "[" "{" ")" "]" "}")))
-                  (split-string value "" 't))))
+  (-pair-helper-p (seq-filter (lambda (x) (member x -all-parentheses))
+                              (split-string value "" 't))))
 
 
-(defun pair-helper-p (pars)
+(defun -pair-helper-p (pars)
   "Check if PARS, a list of parentheses chars, are correctly matched."
   (let ((stack))
     (catch 'mismatch
       (dolist (elem pars (null stack))
-        (message "elem: %s stack: %s" elem stack)
-        (if (member elem '("(" "[" "{"))
-            (setq stack (cons elem stack))
-          (if (not (paired-parens-p (car stack) elem))
-              (throw 'mismatch nil)
-            (setq stack (cdr stack))))))))
+        (if (member elem -opening-parentheses)
+            (push elem stack)
+          (unless (-paired-parens-p (car stack) elem)
+            (throw 'mismatch nil))
+          (pop stack))))))
 
 
-(defun paired-parens-p (par-left par-right)
+(defun -paired-parens-p (par-left par-right)
   "Check if PAR-LEFT and PAR-RIGHT are matching parens."
-  (member (pair par-left par-right) '(("(" ")") ("[" "]") ("{" "}"))))
+  (member (list par-left par-right) parentheses-pairs))
 
 
-(defun pair (a b)
-  "Construct a pair (a list) out of A and B."
-  (cons a (cons b nil)))
+(defconst parentheses-pairs
+  '(("(" ")") ("[" "]") ("{" "}"))
+  "Pairs of matching parentheses.")
 
+(defconst -all-parentheses
+  (flatten-list parentheses-pairs)
+  "All parentheses characters.")
 
-(defun filter-dolist (pred list)
-  "Filter LIST using PRED."
-  (let (result)
-    (dolist (element (reverse list) result)
-      (if (apply pred element ())
-          (setq result (cons element result))))))
+(defconst -opening-parentheses
+  (mapcar #'car parentheses-pairs)
+  "Character symbols of opening (left) parentheses.")
 
 
 (provide 'matching-brackets)
