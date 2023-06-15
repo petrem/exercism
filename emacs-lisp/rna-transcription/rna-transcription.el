9a310d798a088016b7a70b7cdb8936546a900f49
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-06-15 18:08:48 +0300

    exercism:emacs-lisp:rna-transcription

--- emacs-lisp/rna-transcription/rna-transcription.el
+++ emacs-lisp/rna-transcription/rna-transcription.el
@@ -4,12 +4,24 @@
 
 ;;; Code:
 
+(defun rna-transcript--translate-nucleotide (nucleotide)
+  "Transform one dna NUCLEOTIDE (which must be a character) to its rna counterpart."
+  (defconst dna-to-rna-map  '((?G . "C") (?C . "G") (?T . "A") (?A . "U")))
+  (or
+   (alist-get nucleotide dna-to-rna-map nil nil '=)
+   (error (format "Unknown nucleotide: %s" nucleotide))))
+  
+
+(rna-transcript--translate-nucleotide ?G)
+
 (defun to-rna (strand)
   "Translate ADN nucleotides in STRAND to RNA."
-  (concat
-   (mapcar
-    (lambda (n) (cdr (assoc n '((?G . ?C) (?C . ?G) (?T . ?A) (?A . ?U)))))
-    strand)))
+  (mapconcat
+    #'rna-transcript--translate-nucleotide
+    strand
+    ""))
+
+
 
 (provide 'rna-transcription)
 ;;; rna-transcription.el ends here
