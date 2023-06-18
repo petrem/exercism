78e720b9e382d946578db0056b2e5cc3989825c1
Petre Mierlutiu <petrem@archeryevent.ro>
2023-06-18 11:29:35 +0300

    exercism:emacs-lisp:anagram

--- emacs-lisp/anagram/anagram.el
+++ emacs-lisp/anagram/anagram.el
@@ -9,20 +9,12 @@
   (let ((subject-lower (downcase subject)))
     (let ((subject-sorted (seq-sort '< subject-lower)))
       (seq-filter
-       (lambda (x)
-         (let ((x-lower (downcase x)))
+       (lambda (candidate)
+         (let ((candidate-lower (downcase candidate)))
            (and
-            (not (string= x-lower subject-lower))
-            (string= (seq-sort '< x-lower) subject-sorted))))
+            (not (string= candidate-lower subject-lower))
+            (string= (seq-sort '< candidate-lower) subject-sorted))))
          candidates))))
-
-
-(defun anagram--letter-counts (subject)
-  "Letter counts of SUBJECT as alist."
-  (seq-sort (lambda (alist1 alist2) (< (car alist1) (car alist2)))
-            (mapcar
-             (lambda (alist) (list (car alist) (1- (length alist))))
-             (seq-group-by #'identity subject))))
 
 (provide 'anagram)
 ;;; anagram.el ends here
