8619e2944250f7caf1b8803b24dc01e0a4a478cb
Petre Mierlutiu <petrem@archeryevent.ro>
2023-07-16 18:35:59 +0300

    exercism:c:pythagorean-triplet

--- c/pythagorean-triplet/pythagorean_triplet.c
+++ c/pythagorean-triplet/pythagorean_triplet.c
@@ -2,19 +2,36 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <errno.h>
 
-/* the "obvious" solution */
+
+/* simple, "obvious", solution, with realloc */
 
 static inline bool is_triplet(triplet_int_t a, triplet_int_t b, triplet_int_t c);
 
 
 triplets_t *triplets_with_sum(triplet_int_t sum) {
-  triplets_t *result = malloc(sizeof(triplets_t) + 100 * sizeof(triplet_t));
+  size_t max_results = 8;
+  triplets_t *result = malloc(sizeof(triplets_t) + max_results * sizeof(triplet_t));
+  if (result == NULL) {
+    exit(ENOMEM);
+  }
   result -> count = 0;
-  for (triplet_int_t a=1; a < sum; a++) {
-    for (triplet_int_t b=a; b < sum; b++) {
+  if (sum < 12) {
+    /* smallest pythagorean triplet is 3, 4, 5 */
+    return result;
+  }
+  for (triplet_int_t a = 3; a < sum - 3; a++) {
+    for (triplet_int_t b = a + 1 ; b < sum - 2; b++) {
       if (is_triplet(a, b, sum - a - b)) {
         result->triplets[result->count++] = (triplet_t){a, b, sum - a - b};
+        if (result->count >= max_results) {
+          max_results *= 2;
+          result = realloc(result, sizeof(triplets_t) + max_results * sizeof(triplet_t));
+          if (result == NULL) {
+            exit(ENOMEM);
+          }
+        }
       }
     }
   }
