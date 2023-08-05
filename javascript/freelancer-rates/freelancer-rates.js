59ade8145b5638653c81bed102df1d219800aaff
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-05 13:44:11 +0300

    exercism:javascript:freelancer-rates

---
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return 8 * ratePerHour;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour));
+}
+
+const BILLABLE_DAYS_IN_MONTH = 22;
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  var fullDays = numDays % BILLABLE_DAYS_IN_MONTH;
+  var discountedDays = numDays - fullDays;
+  return Math.ceil(
+    dayRate(ratePerHour * (1-discount)) * discountedDays
+      + dayRate(ratePerHour) * fullDays);
+}
