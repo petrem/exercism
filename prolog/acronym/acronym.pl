02bb247af7b39b3d571ff2473e2bfdf6b1f473c1
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-16 09:41:06 +0300

    exercism:prolog:acronym

--- prolog/acronym/acronym.pl
+++ prolog/acronym/acronym.pl
@@ -1,1 +1,16 @@
-abbreviate(Sentence, Acromym).
+abbreviate(Sentence, Acronym):-
+    separators(Sep),
+    string_upper(Sentence, SentenceUpper)
+    , split_string(SentenceUpper, Sep, Sep, Words)
+    , words_heads(Words, Heads)
+    , string_chars(Acronym, Heads).
+
+separators(" _-").
+
+words_heads([], []).
+words_heads([Word|Rest], [Head|Heads]):-
+    word_head(Word, Head)
+    , words_heads(Rest, Heads).
+
+word_head(Word, Head):-
+    string_chars(Word, [Head|_]).
