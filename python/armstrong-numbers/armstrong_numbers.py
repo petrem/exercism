417872e18d01ac895f4f9630fab892bd70bd0dfa
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-21 18:04:54 +0300

    exercism:python:armstrong-numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,4 +1,4 @@
-def is_armstrong(number):
-    num_s = f"{number}"
+def is_armstrong_number(number):
+    num_s = str(number)
     digits = len(num_s)
     return number == sum(int(d) ** digits for d in num_s)
