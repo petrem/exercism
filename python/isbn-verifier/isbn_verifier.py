cb0e4a543bbbd03dde0088bf3b1f59d3c0f2cd79
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-23 16:26:32 +0300

    execism:python:isbn-verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,20 +1,18 @@
-from itertools import count
+"""The solution iterates through the string just once. Otherwise, not particularly nice."""
+from itertools import count, islice
 from operator import mul
 
 
-def verify(isbn):
-    try:
-        control = 10 if isbn[-1] == "X" else int(isbn[-1])
-    except (ValueError, IndexError):
-        return False
+def is_valid(isbn):
+    """Validate ISBN-10 string."""
+    digits = (d for d in isbn if d != "-")
     counter = count(10, -1)
-    checksum = sum(
-        map(
-            mul,
-            (int(d) for d in isbn[:-1] if d.isdigit()),
-            counter,
-        )
-    ) + control
-    if next(counter) != 1:
+    try:
+        checksum = sum(map(mul, (int(d) for d in islice(digits, 9)), counter))
+        control = next(digits)
+        checksum += 10 if control == "X" else int(control)
+        if next(counter) != 1 or next(digits, None) is not None:
+            return False
+    except (StopIteration, ValueError):
         return False
     return checksum % 11 == 0
