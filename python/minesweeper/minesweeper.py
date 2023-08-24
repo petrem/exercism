79a0d90976f175269327f9d0e0343281fc09010f
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-24 13:38:34 +0300

    exercism:python:minesweeper

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,24 +1,24 @@
-from typing import List, Tuple
+from itertools import product
+from typing import Iterable
 
-Minefield = List[str]
+
+Minefield = list[str]
 
 
 def annotate(minefield: Minefield) -> Minefield:
-    # Function body starts here
     board = Board(minefield)
     board.annotate()
     return board.to_minefield()
 
 
 class Board:
-
     def __init__(self, minefield: Minefield) -> None:
         self._validate_minefield(minefield)
-        self.board = [[c for c in row] for row in minefield]
+        self.board = [list(row) for row in minefield]
         self.n_rows = len(minefield)
         self.n_cols = len(minefield[0]) if self.n_rows else 0
 
-    def annotate(self):
+    def annotate(self) -> None:
         for x in range(self.n_rows):
             for y in range(self.n_cols):
                 if not self.is_mine(x, y):
@@ -31,9 +31,9 @@ class Board:
         if not all(isinstance(s, str) for s in minefield):
             raise ValueError("The minefield contains non-str rows")
         if not all(len(s) == len(minefield[0]) for s in minefield):
-            raise ValueError("The minefield contains rows of different length")
+            raise ValueError("The board is invalid with current input.")
         if any(set(s) - set(" *") for s in minefield):
-            raise ValueError("The minefield contains invalid characters")
+            raise ValueError("The board is invalid with current input.")
 
     def is_on_board(self, x: int, y: int) -> bool:
         return 0 <= x < self.n_rows and 0 <= y < self.n_cols
@@ -41,19 +41,14 @@ class Board:
     def is_mine(self, x: int, y: int) -> bool:
         return self.board[x][y] == "*"
 
-    def cell_neighbors(self, x: int, y: int) -> List[Tuple[int, int]]:
-        return [
-            (x + delta_x, y + delta_y)
-            for delta_x in range(-1, 2)
-            for delta_y in range(-1, 2)
-            if (
-                self.is_on_board(x + delta_x, y + delta_y)
-                and (delta_x != 0 or delta_y != 0)
-            )
-        ]
+    def cell_neighbors(self, x: int, y: int) -> Iterable[tuple[int, int]]:
+        for dx, dy in product([-1, 0, 1], [-1, 0, 1]):
+            coord = x + dx, y + dy
+            if self.is_on_board(*coord) and (dx or dy):
+                yield coord
 
     def count_neighbor_mines(self, x: int, y: int) -> int:
-        return sum(self.is_mine(i, j) for (i, j) in self.cell_neighbors(x, y))
+        return sum(self.is_mine(i, j) for i, j in self.cell_neighbors(x, y))
 
     def to_minefield(self) -> Minefield:
         return [''.join(row) for row in self.board]
