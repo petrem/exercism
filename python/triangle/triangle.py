1b9c18a42b913a85e705645ffbbe9e53800ed362
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-24 15:16:17 +0300

    exercism:python:triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -12,13 +12,13 @@ def _is_triangle(sides):
     return all(s > 0 for s in sides) and all(i + j > k for i, j, k in two_by_two(sides))
 
 
-def is_equilateral(sides):
+def equilateral(sides):
     return sides[0] > 0 and set(sides) == set([sides[0]])
 
 
-def is_isosceles(sides):
+def isosceles(sides):
     return _is_triangle(sides) and any(i == j for i, j, k in two_by_two(sides))
 
 
-def is_scalene(sides):
-    return _is_triangle(sides) and not is_isosceles(sides)
+def scalene(sides):
+    return _is_triangle(sides) and not isosceles(sides)
