f5afb86fc04dd08891fd05f63251eec08e31089b
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-30 12:45:05 +0300

    update erlang boilerplate and remove extra curly bracket in generated commit messages

--- erlang/shell.nix
+++ erlang/shell.nix
@@ -1,21 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-let
-  inherit (lib) optional optionals;
-in
-
-mkShell {
-  buildInputs = [
-    (import ./nix/default.nix { inherit pkgs; })
-    entr
-    exercism
-    git
-    just
-  ] ++ optional stdenv.isLinux inotify-tools
-  ++ optional stdenv.isDarwin terminal-notifier
-  ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-    CoreFoundation
-    CoreServices
-  ]);
-}
+let findNixDir = dir:
+      let nixDir = "${dir}/nix" ;
+      in if builtins.pathExists nixDir
+         then nixDir
+         else
+           if dir == "/"
+           then abort "Cannot find a nix directory uphill"
+           else findNixDir (dirOf dir);
+    dirName = baseNameOf ./.;
+    track = if dirName != "exercism"
+            then dirName
+            else null;
+in (import (findNixDir ./. + "/default.nix") {inherit track;}).shell
