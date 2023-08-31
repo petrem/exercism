e10696d41bf30b68c46b523a6279c2d2b8224595
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-31 15:50:12 +0300

    exercism:python:triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,24 +1,29 @@
+"""triangle"""
 from itertools import combinations, starmap
 
 
-def two_by_two(sides):
+def _two_by_two(sides):
     return starmap(
-        lambda x, y: (sides[x], sides[y], sides[3 - x - y]),
-        combinations(range(3), 2)
+        lambda x, y: (sides[x], sides[y], sides[3 - x - y]), combinations(range(3), 2)
     )
 
 
 def _is_triangle(sides):
-    return all(s > 0 for s in sides) and all(i + j > k for i, j, k in two_by_two(sides))
+    return all(s > 0 for s in sides) and all(
+        i + j > k for i, j, k in _two_by_two(sides)
+    )
 
 
 def equilateral(sides):
-    return sides[0] > 0 and set(sides) == set([sides[0]])
+    """Is equilateral?"""
+    return sides[0] > 0 and set(sides) == {sides[0]}
 
 
 def isosceles(sides):
-    return _is_triangle(sides) and any(i == j for i, j, k in two_by_two(sides))
+    """Is isosceles?"""
+    return _is_triangle(sides) and any(i == j for i, j, k in _two_by_two(sides))
 
 
 def scalene(sides):
+    """Is scalene?"""
     return _is_triangle(sides) and not isosceles(sides)
