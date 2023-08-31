4eda83f49de4e42f47aa80f234c2b57fbb20e17e
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-08-31 17:30:34 +0300

    exercism:javascript:resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,17 +1,14 @@
-export const value = (colorBandArray) => {
+export const decodedValue = (colorBandArray) => {
     return colorCode(colorBandArray[0]) * 10 + colorCode(colorBandArray[1]);
 };
 
 // modified version of 'resistor-color' that returns NaN as an error instead of null
 
 export const colorCode = (color) => {
-    if (typeof(color) === "string") {
-        let val = COLORS.indexOf(color.toLowerCase());
-        if (val >= 0)
-            return val;
-    }
-    return NaN;
+  let val = COLORS.indexOf(color.toLowerCase());
+  return val >= 0 ? val : NaN;
 };
+
 
 export const COLORS = [
     "black"
