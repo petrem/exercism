692d42aa9c38af93e2453d18d0d530bcba24172a
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-09-01 20:07:19 +0300

    exercism:haskell:isbn-verifier

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,6 @@
-module IsbnVerifier (isbn) where
+module IsbnVerifier
+  ( isbn
+  ) where
 
 import qualified Data.Char as C
 import Data.Monoid
@@ -7,14 +9,20 @@ isbn :: String -> Bool
 isbn [] = False
 isbn xs = ifAllTrue isbnChecks
   where
-    isbnChecks = [all (\x -> C.isDigit x || x == '-') $ init xs
-                 ,length digits == 9
-                 ,C.isDigit (last xs) || C.toLower (last xs) == 'x'
-                 ,checkSum `mod` 11 == 0
-                 ]
-    digits  = map C.digitToInt $ filter C.isDigit $ init xs
-    control = (\x -> if C.isDigit x then C.digitToInt x else 10) $ last xs
-    checkSum = sum $ zipWith (*) [1..10] (control:reverse digits)
+    isbnChecks =
+      [ all (\x -> C.isDigit x || x == '-') $ init xs
+      , length digits == 9
+      , C.isDigit (last xs) || C.toLower (last xs) == 'x'
+      , checkSum `mod` 11 == 0
+      ]
+    digits = map C.digitToInt $ filter C.isDigit $ init xs
+    control =
+      (\x ->
+         if C.isDigit x
+           then C.digitToInt x
+           else 10) $
+      last xs
+    checkSum = sum $ zipWith (*) [1 .. 10] (control : reverse digits)
 
 ifAllTrue :: [Bool] -> Bool
 ifAllTrue = getAll . mconcat . map All
