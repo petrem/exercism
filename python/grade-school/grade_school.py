079961b0097de94745e16f83577448093613e62c
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-09-01 16:16:27 +0300

    exercism:python:grade-school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,19 +1,68 @@
-from collections import defaultdict
+"""Exercism: python: grade-school
+
+What I'd expect to be a typical solution.
+"""
+from bisect import insort
 
 
 class School:
+    """School grades roster."""
+
     def __init__(self):
-        self._rosters = defaultdict(list)
+        self._rosters = {}
+        self._added = []
 
     def add_student(self, name, grade):
-        self._rosters[grade].append(name)
+        """Add student to the roster, if not already present.
+
+        Records a trail of successfull additions.
+
+        :param name: Student name.
+        :type name: str
+
+        :param grade: Grade for the student.
+        :type grade: int
+
+        :return: Nothing.
+        :rtype: None
+        """
+
+        if name not in self.roster():
+            roster = self._rosters.setdefault(grade, [])
+            insort(roster, name)
+            self._added.append(True)
+        else:
+            self._added.append(False)
 
     def roster(self):
+        """Get the full list of students from the roster.
+
+        :return: List of students, ordered by grade and name.
+        :rtype: list[str]
+        """
         return [
-            student
-            for grade in sorted(self._rosters.keys())
-            for student in self.grade(grade)
+            name for grade in sorted(self._rosters.keys()) for name in self.grade(grade)
         ]
 
     def grade(self, grade):
-        return sorted(self._rosters[grade])
+        """Get students having the specified grade.
+
+        :param grade: The grade for which a roster is requested.
+        :type grade: int
+
+        :return: The list of students for the specified grade.
+        :rtype: list[str]
+        """
+
+        if grade in self._rosters:
+            return list(self._rosters[grade])
+        return []
+
+    def added(self):
+        """Get the trail for :method:``add_student``.
+
+        :return: List of bools indicating successful additions.
+        :rtype: list[bool]
+        """
+
+        return list(self._added)
