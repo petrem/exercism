e19692d4085663905610c0ae131377df6b255c51
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-10-01 09:06:13 +0300

    exercism:python:bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,25 +1,28 @@
-def is_question(phrase):
+"""Bob."""
+
+
+def response(phrase):
+    """Respond lackadaisically."""
+    cooked_phrase = phrase.strip()
+    if _is_silence(cooked_phrase):
+        return "Fine. Be that way!"
+    if _is_shout(cooked_phrase):
+        if _is_question(cooked_phrase):
+            return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+    if _is_question(cooked_phrase):
+        return "Sure."
+    return "Whatever."
+
+
+def _is_question(phrase):
     return phrase.endswith("?")
 
 
-def is_shout(phrase):
+def _is_shout(phrase):
     # I just learned isupper() ignores non-alpha characters -- weird!
     return phrase.isupper()
 
 
-def is_silence(phrase):
+def _is_silence(phrase):
     return phrase == ""
-
-
-def hey(phrase):
-    cooked_phrase = phrase.strip()
-    if is_silence(cooked_phrase):
-        return "Fine. Be that way!"
-    if is_shout(cooked_phrase):
-        if is_question(cooked_phrase):
-            return "Calm down, I know what I'm doing!"
-        else:
-            return "Whoa, chill out!"
-    if is_question(cooked_phrase):
-        return "Sure."
-    return "Whatever."
