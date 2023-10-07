77ab3e3e0ceeb0093e0e98b18f31eb951899088a
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-10-07 12:15:32 +0300

    exercism:elixir:guessing-game

---
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -0,0 +1,9 @@
+defmodule GuessingGame do
+  def compare(secret_number, guess\\:no_guess)
+  def compare(_secret_number, _guess) when _guess == :no_guess, do: "Make a guess"
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+  def compare(_secret_number, _guess), do: "Sorry, I don't understand"
+end
