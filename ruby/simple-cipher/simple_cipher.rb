fa6781fd2cadcdbe154d9d976c219864b1ee7a78
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-10-12 17:42:19 +0300

    exercism:ruby:simple-cipher

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -1,20 +1,50 @@
 class Cipher
   RAND_KEY_LEN = 100
-  ALPHABET = [*'a'..'z']
+  ALPHABET = ('a'..'z').to_a
   DELTA = ALPHABET[0].ord
 
-  def to_charcodes(xs)
-    (xs.respond_to?("split") ? xs.split("") : xs).map(&:ord)
+  def initialize(key=nil)
+    if key.nil?
+      @key = to_charcodes(
+        (1..RAND_KEY_LEN).map {|_| ALPHABET[Random.rand(ALPHABET.size)]})
+    else
+      if not key.match(/^[[:lower:]]+$/)
+        raise ArgumentError.new("we don't like your kind of key around here")
+      end
+      @key = to_charcodes_from_s(key)
+    end
   end
 
+  def decode(cipher)
+    charcodes_to_s zip_with(to_charcodes_from_s(cipher), @key.cycle) { |x, y| subtract_in_cipher(x, y) }
+  end
+
+  def encode(plain)
+    charcodes_to_s zip_with(to_charcodes_from_s(plain), @key.cycle) { |x, y| add_in_cipher(x, y) }
+  end
+
+  def key
+    charcodes_to_s(@key)
+  end
+
+  private
+
+  def to_charcodes(xs)
+    xs.map(&:ord)
+  end
+
+  def to_charcodes_from_s(s)
+    to_charcodes(s.split "")
+  end
+  
   def charcodes_to_s(charcodes)
     charcodes.map(&:chr).join
   end
 
-  # Would be nice to be able to use functions, or Proc, but it did't work. I'm missing
-  # something.
-  def zip_with(f_id, xs, ys)
-    xs.zip(ys).map {|(x, y)| self.send(f_id, x, y)}
+  def zip_with(xs, ys)
+    raise ArgumentError, "block must be passed in as an argument" unless block_given?
+
+    xs.zip(ys).map { |(x, y)| yield x, y }
   end
 
   def add_in_cipher(x, y)
@@ -26,28 +56,4 @@ class Cipher
     DELTA + (x - y).modulo(ALPHABET.size)
   end
 
-  def initialize(key=nil)
-    if key.nil? then
-      @key = to_charcodes(
-        (1..RAND_KEY_LEN).map {|_| ALPHABET[Random.rand(ALPHABET.size)]})
-    else
-      if not key.match(/^[[:lower:]]+$/) then
-        raise ArgumentError.new("we don't like your kind of key around here")
-      end
-      @key = to_charcodes(key)
-    end
-  end
-
-  def key
-    charcodes_to_s(@key)
-  end
-
-
-  def encode(plain)
-    charcodes_to_s(zip_with(:add_in_cipher, to_charcodes(plain), @key.cycle))
-  end
-
-  def decode(cipher)
-    charcodes_to_s(zip_with(:subtract_in_cipher, to_charcodes(cipher), @key.cycle))
-  end
 end
