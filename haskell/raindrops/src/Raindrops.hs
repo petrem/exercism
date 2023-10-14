2c9bf1a06c8c8d0d93a73e502f7170f8381c88f0
Petre Mierlutiu <petrem@users.noreply.github.com>
2023-10-14 16:03:29 +0300

    wip

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,20 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Raindrops (convert) where
 
 import Data.Maybe  (fromMaybe)
 import qualified Data.Text as T
 import Data.Text (Text)
 
-convert :: Integral a => a -> Text
-convert n = fromMaybe untranslatable (toRainSpeak raindropFromInt rainVocabulary n)
-  where
-    untranslatable = T.unpack . show $ n
-    maybeDroplet rain = bool Nothing (Just rain) . dividesBy n
-      
-    dividesBy 0 _ = False
-    dividesBy y x = x `rem` y == 0
 
-    isDivisibleBy _ 0 = False
-    isDivisibleBy x y = x `rem` y == 0
+-- A solution that attempts a bit of generalization.
+{-
+Assuming
+- there could be multiple "rain words",
+- the mapping from the input to "rain words" should be flexible,
+- construction of the "rain phrase" is still an ordered accumulation of words,
+
+-}
+
+
+convert :: (Show a, Integral a) => a -> Text
+convert n = undefined -- fromMaybe untranslatable $ foldMap (maybeWord n) rainDict
+  where
+    untranslatable = T.pack . show $ n
+
+    maybeWord :: a -> b -> Maybe Text
+    maybeWord = undefined
+      
+    divides _ 0 = False
+    divides y x = x `rem` y == 0
+
+    rainDict = undefined
+
+mkWordTranslator :: (b -> a -> Bool) -> a -> b -> Maybe b
+mkWordTranslator p from to = if p from to then Just to else Nothing
+
+data RainTranslator a b = RainTranslator { runPred :: a -> Bool
+                                         , getRain :: b
+                                         }
+
 
 data Droplet a b = Droplet a b deriving (Eq, Show)
 
@@ -23,16 +44,17 @@ type RainVocabulary t a b = t (a, b)
 rainVocabulary :: RainVocabulary [] Int String
 rainVocabulary = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
 
-toRainSpeak ::
-     (Foldable t, Integral a, Monoid b)
-  => (a -> Bool)
-  -> RainVocabulary t a b
-  -> a
-  -> Maybe b
-toRainSpeak p ws n = foldMap (\(k, repr) -> if dividesBy k n then Just repr else Nothing) ws
+-- toRainSpeak ::
+--      (Foldable t, Integral a, Monoid b)
+--   => (a -> Bool)
+--   -> RainVocabulary t a b
+--   -> a
+--   -> Maybe b
+-- toRainSpeak p ws n = foldMap (\(k, repr) -> if dividesBy k n then Just repr else Nothing) ws
 
 
-{- A simple, obvious, version using String monoid
+{-
+-- A simple, obvious version where the String monoid's `mempty` is flagging failure.
 
 convert :: Int -> String
 convert n = let converted = foldMap go [(3, "Pling"), (5, "Plang"), (7, "Plong")]
@@ -42,7 +64,9 @@ convert n = let converted = foldMap go [(3, "Pling"), (5, "Plang"), (7, "Plong")
 -}
 
 
-{- Original solution that plays with IntMap, but this isn't useful.
+{-
+
+-- Original solution that plays with IntMap. Not too useful.
 
 import Data.IntMap (IntMap, fromList, (!), keys)
 import Data.Maybe  (fromMaybe)
