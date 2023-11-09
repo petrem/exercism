1b4577b1af87315f6ceac43263fcb10716ab9e96
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-11-10 01:07:37 +0200

    exercism:javascript:pascals-triangle

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,20 +1,15 @@
 export const rows = (n) => {
-  if (n < 1) {
-    return [];
-  }
-  return [...range(n-1)].reduce(
-    (acc, _) => acc.concat([next_row(last(acc))]),
-    [[1]]
-  );
+    return (n < 1) ? [] : [...range(n-1)].reduce(
+        (acc, _) => acc.concat([next_row(last(acc))]), [[1]]  //eslint-disable-line no-unused-vars
+    );
 };
 
 const next_row = (row) => zipWith(
-  (a, b) => a + b,
-  [[0, ...row], [...row, 0]]
+  (a, b) => a + b, [[0, ...row], [...row, 0]]
 );
 
-const zipWith = (op, rows) => rows[0].map(
-  (_, idx) => (rows.map((row) => row[idx])).reduce(op)
+const zipWith = (op, rs) => rs[0].map(
+  (_, idx) => (rs.map((r) => r[idx])).reduce(op)
 );
 
 const range = (n) => Array(n).keys(); // stolen from https://stackoverflow.com/a/10050831
