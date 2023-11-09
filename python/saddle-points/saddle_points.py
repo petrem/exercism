50e203901de636e0d9d0a3936e600dd25658b764
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-11-09 21:34:25 +0200

    exercism:python:saddle-points

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,12 +1,14 @@
+"""Exercise: saddle-points"""
 from collections import defaultdict
 
 
 def saddle_points(matrix):
+    """Find saddle points in matrix."""
     if matrix == []:
-        return set()
+        return []
     n_cols = len(matrix[0])
     if any(len(row) != n_cols for row in matrix[1:]):
-        raise ValueError("Irregular matrix")
+        raise ValueError("irregular matrix")
     rows_candidates = defaultdict(set)
     cols_candidates = defaultdict(set)
     cols_min = list(matrix[0])
@@ -15,15 +17,16 @@ def saddle_points(matrix):
         for j, elem in enumerate(row, start=1):
             if elem > row_max:
                 row_max = elem
-                rows_candidates[i] = set([(i, j)])
+                rows_candidates[i] = {(i, j)}
             elif elem == row_max:
                 rows_candidates[i].add((i, j))
             if elem < cols_min[j - 1]:
                 cols_min[j - 1] = elem
-                cols_candidates[j] = set([(i, j)])
+                cols_candidates[j] = {(i, j)}
             elif elem == cols_min[j - 1]:
                 cols_candidates[j].add((i, j))
-    return set.intersection(
+    result = set.intersection(
         set.union(*rows_candidates.values()),
         set.union(*cols_candidates.values()),
     )
+    return [{"row": r, "column": c} for (r, c) in result]
