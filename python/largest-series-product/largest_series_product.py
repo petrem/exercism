500abdedb1e4b34f1c0d22800d5d1bd5bd5c8ac1
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-11-09 21:22:43 +0200

    exercism:python:largest-series-product

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,3 +1,4 @@
+"""Exercise: largest-series-product"""
 from collections import deque
 from functools import reduce
 from itertools import islice
@@ -5,16 +6,22 @@ from operator import mul
 
 
 def largest_product(series, size):
+    """Find largest series product with specified size."""
+    if size < 0:
+        raise ValueError("span must not be negative")
     series_iterator = iter(int(d) for d in series)
     window = deque(islice(series_iterator, size), size)
     if len(window) < size:
-        raise ValueError("Input series shorter than window size")
-    maximum = product(window)
-    for digit in series_iterator:
-        window.append(digit)
-        maximum = max(maximum, product(window))
+        raise ValueError("span must be smaller than string length")
+    maximum = _product(window)
+    try:
+        for digit in series_iterator:
+            window.append(digit)
+            maximum = max(maximum, _product(window))
+    except ValueError as e:
+        raise ValueError("digits input must only contain digits") from e
     return maximum
 
 
-def product(iterable):
+def _product(iterable):
     return reduce(mul, list(iterable), 1)
