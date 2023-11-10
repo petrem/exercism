4b07f176825e38c1dfc4a808a54bcc65bbda5a0e
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-11-10 19:34:39 +0200

    exercism:javascript:triangle

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,28 +1,26 @@
-//
-// I wouldn't allow creating an invalid triangle, instead of throwing from kind()...
-// but alas, the tests say otherwise.
-//
-
 export class Triangle {
-  constructor(a, b, c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-  }
-
-  kind() {
-    if ([this.a <= 0
-         , this.b <= 0
-         , this.c <= 0
-         , this.a + this.b < this.c
-         , this.a + this.c < this.b
-         , this.b + this.c < this.a
-        ].some(id)) {
-      throw new Error("Invalid triangle");
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
-    let equal_sides = [this.a == this.b, this.b == this.c, this.a == this.c].filter(id).length;
-    return ["scalene", "isosceles", "equilateral", "equilateral"][equal_sides];
-  }
+
+    kind() {
+        if ([this.a <= 0
+             , this.b <= 0
+             , this.c <= 0
+             , this.a + this.b < this.c
+             , this.a + this.c < this.b
+             , this.b + this.c < this.a
+            ].some(id)) {
+            return null;
+        }
+        let equal_sides = [this.a === this.b, this.b === this.c, this.a === this.c].filter(id).length;
+        return ["scalene", "isosceles", "equilateral", "equilateral"][equal_sides];
+    }
+    get isScalene() { return this.kind() === "scalene"; }
+    get isIsosceles() { return this.kind() === "isosceles" || this.kind() === "equilateral"; }
+    get isEquilateral() { return this.kind() === "equilateral"; }
 }
 
 const id = (something) => something;
