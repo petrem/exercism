fc2be1165504bc1ff0f0b5349c1d3779fa5fe614
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2023-12-08 15:08:21 +0200

    exercism:python:plane-tickets

---
+++ python/plane-tickets/generators.py
@@ -0,0 +1,65 @@
+"""Functions to automate Conda airlines ticketing system."""
+from itertools import chain, count, cycle, islice
+
+
+def generate_seat_letters(number):
+    """Generate a series of letters for airline seats.
+
+    :param number: int - total number of seat letters to be generated.
+    :return: generator - generator that yields seat letters.
+
+    Seat letters are generated from A to D.
+    After D it should start again with A.
+
+    Example: A, B, C, D
+
+    """
+    yield from islice(cycle("ABCD"), number)
+
+
+def generate_seats(number):
+    """Generate a series of identifiers for airline seats.
+
+    :param number: int - total number of seats to be generated.
+    :return: generator - generator that yields seat numbers.
+
+    A seat number consists of the row number and the seat letter.
+
+    There is no row 13.
+    Each row has 4 seats.
+
+    Seats should be sorted from low to high.
+
+    Example: 3C, 3D, 4A, 4B
+
+    """
+    yield from islice(
+        (f"{n}{letter}" for n in chain(range(1, 13), count(14)) for letter in "ABCD"),
+        number,
+    )
+
+
+def assign_seats(passengers):
+    """Assign seats to passengers.
+
+    :param passengers: list[str] - a list of strings containing names of passengers.
+    :return: dict - with the names of the passengers as keys and seat numbers as values.
+
+    Example output: {"Adele": "1A", "Björk": "1B"}
+
+    """
+    seats = generate_seats(len(passengers))
+
+    return {p: next(seats) for p in passengers}
+
+
+def generate_codes(seat_numbers, flight_id):
+    """Generate codes for a ticket.
+
+    :param seat_numbers: list[str] - list of seat numbers.
+    :param flight_id: str - string containing the flight identifier.
+    :return: generator - generator that yields 12 character long ticket codes.
+
+    """
+
+    yield from (f"{sn + flight_id:012}" for sn in seat_numbers)
