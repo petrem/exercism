faaf2eda5229f9b559668b984b2ca47d317366be
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2024-02-04 13:41:57 +0200

    exercism:emacs-lisp:raindrops

--- emacs-lisp/raindrops/raindrops.el
+++ emacs-lisp/raindrops/raindrops.el
@@ -3,31 +3,27 @@
 ;;; Commentary:
 
 ;;; Code:
-(defun convert (n)
-  "Convert integer N to its raindrops string."
-  (let* ((as-rain (cl-reduce
-                   (lambda (acc rain-word)
-                     (cons (if (eq 0 (% n (car rain-word))) (cdr rain-word))
-                           acc))
-                   rainspeak :initial-value () ))
-         (as-rain-str (string-join (reverse as-rain))))
-    (if (string-empty-p as-rain-str)
-        (number-to-string n)
-      as-rain-str))
-)
 
-(defconst rainspeak '((3 . "Pling") (5 . "Plang") (7 . "Plong")))
+(defconst raindrops--rainspeak '([3 "Pling"] [5 "Plang"] [7 "Plong"]))
 
-(defsubst string-empty-p (string)
-  "Check whether STRING is empty."
-  (string= string ""))
-
-(defsubst string-join (strings)
+(defsubst raindrops--string-join (strings)
   "Join all STRINGS."
   (mapconcat #'identity strings nil))
 
-(require 'cl-lib)
-(require 'cl-seq)
+(defun convert (n)
+  "Convert integer N to its raindrops string."
+  (let* ((as-rain
+          (seq-reduce
+           (lambda (acc num-word)
+             (seq-let [num word] num-word
+               (cons (if (eq 0 (% n num)) word) acc)))
+           raindrops--rainspeak
+           () ))
+         (as-rain-str (raindrops--string-join (reverse as-rain))))
+    (if (seq-empty-p as-rain-str)
+        (number-to-string n)
+      as-rain-str))
+)
 
 (provide 'raindrops)
 ;;; raindrops.el ends here
