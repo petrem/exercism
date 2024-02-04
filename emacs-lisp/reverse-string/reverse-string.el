e85d1e24ee48da015b4f6a101da21ac84df00d26
Petre Mierlutiu <2434132+petrem@users.noreply.github.com>
2024-02-04 16:24:02 +0200

    exercism:emacs-lisp:reverse-string

--- emacs-lisp/reverse-string/reverse-string.el
+++ emacs-lisp/reverse-string/reverse-string.el
@@ -7,15 +7,22 @@
 ;;; Code:
 
 
+(defsubst reverse-string--swap (arr idx1 idx2)
+  "Swap elements at IDX1 and IDX2 in ARR."
+  (let ((tmp (aref arr idx1)))
+    (aset arr idx1 (aref arr idx2))
+    (aset arr idx2 tmp)))
+
 (defun reverse-string (value)
   "Reverse VALUE."
-  (seq-into (reverse-string--reverse-list (seq-into value 'list) ()) 'string))
-
-(defun reverse-string--reverse-list (value result)
-  (if (null value)
-      result
-    (reverse-string--reverse-list (cdr value) (cons (car value) result))))
-
+  (let* ((copy (copy-sequence value))
+         (l-idx 0)
+         (r-idx (1- (length value))))
+    (while (< l-idx r-idx)
+      (reverse-string--swap copy l-idx r-idx)
+      (setq l-idx (1+ l-idx))
+      (setq r-idx (1- r-idx)))
+    copy))
 
 (provide 'reverse-string)
 ;;; reverse-string.el ends here
