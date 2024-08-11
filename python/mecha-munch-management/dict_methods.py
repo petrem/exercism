09b95760c2f242b0397c1dad7ac3bc1e27ba0497
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-08-12 00:27:12 +0300

    exercism:python:mecha-munch-management

---
+++ python/mecha-munch-management/dict_methods.py
@@ -0,0 +1,75 @@
+"""Functions to manage a users shopping cart items."""
+from collections import Counter
+
+
+def add_item(current_cart, items_to_add):
+    """Add items to shopping cart.
+
+    :param current_cart: dict - the current shopping cart.
+    :param items_to_add: iterable - items to add to the cart.
+    :return: dict - the updated user cart dictionary.
+    """
+
+    c = Counter(current_cart)
+    c.update(items_to_add)
+    return dict(c)
+
+
+def read_notes(notes):
+    """Create user cart from an iterable notes entry.
+
+    :param notes: iterable of items to add to cart.
+    :return: dict - a user shopping cart dictionary.
+    """
+
+    return dict(Counter(notes))
+
+
+def update_recipes(ideas, recipe_updates):
+    """Update the recipe ideas dictionary.
+
+    :param ideas: dict - The "recipe ideas" dict.
+    :param recipe_updates: dict - dictionary with updates for the ideas section.
+    :return: dict - updated "recipe ideas" dict.
+    """
+
+    new_ideas = ideas.copy()
+    new_ideas.update(recipe_updates)
+    return new_ideas
+
+
+def sort_entries(cart):
+    """Sort a users shopping cart in alphabetically order.
+
+    :param cart: dict - a users shopping cart dictionary.
+    :return: dict - users shopping cart sorted in alphabetical order.
+    """
+
+    if cart is not None:
+        return dict(sorted(cart.items()))
+    return None
+
+
+def send_to_store(cart, aisle_mapping):
+    """Combine users order to aisle and refrigeration information.
+
+    :param cart: dict - users shopping cart dictionary.
+    :param aisle_mapping: dict - aisle and refrigeration information dictionary.
+    :return: dict - fulfillment dictionary ready to send to store.
+    """
+
+    return {k: [cart[k], *aisle_mapping[k]] for k in sorted(cart, reverse=True)}
+
+
+def update_store_inventory(fulfillment_cart, store_inventory):
+    """Update store inventory levels with user order.
+
+    :param fulfillment cart: dict - fulfillment cart to send to store.
+    :param store_inventory: dict - store available inventory
+    :return: dict - store_inventory updated.
+    """
+
+    return {
+        k: [max(v[0] - fulfillment_cart.get(k, [0])[0], 0) or "Out of Stock", *v[1:]]
+        for k, v in store_inventory.items()
+    }
