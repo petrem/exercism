4bed156606fc6e220a4d0373cc0fedd9ba54c4c8
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-08-15 16:52:10 +0300

    exercism:python:raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,19 +1,55 @@
-"""Raindrops."""
-from collections import namedtuple
+"""Raindrops (ursoidea version)."""
+from collections.abc import Iterable
+from functools import singledispatchmethod
+from itertools import compress
+from typing import Self
 
 
-RainSpeak = namedtuple("RainSpeak", "factor, translation")
-RAINSPEAK = [
-    RainSpeak(3, "Pling"),
-    RainSpeak(5, "Plang"),
-    RainSpeak(7, "Plong"),
-]
+class NamedInt(int):
+    """Ints with names.
+
+    This is a gimmick, among others because operations yield simple ints.
+    """
+
+    def __new__(cls, name, *args):
+        value = super().__new__(cls, *args)
+        value.name = name
+        return value
+
+    def __str__(self):
+        return self.name  # pylint: disable=no-member
+
+
+class Vector(list):
+    """Simpleton relative of some Pandas and other bears."""
+
+    @singledispatchmethod
+    def __getitem__(self, index):
+        return super().__getitem__(index)
+
+    @__getitem__.register
+    def _(self, selectors: Iterable) -> Self:
+        return self.__class__(compress(self, selectors))
+
+    def __rmod__(self, other):
+        return self.__class__(other % x for x in self)
+
+    @singledispatchmethod
+    def __eq__(self, other):
+        return self.__class__(x == other for x in self)
+
+    @__eq__.register
+    def _(self, other: Iterable):
+        return super().__eq__(other)
+
+    def __str__(self):
+        return "".join(map(str, self))
+
+
+RAINSPEAK = Vector([NamedInt("Pling", 3), NamedInt("Plang", 5), NamedInt("Plong", 7)])
 
 
 def convert(number):
-    """Convert to raindrop-speak."""
+    """Convert `number` to raindrop-speak."""
 
-    return (
-        "".join(rs.translation for rs in RAINSPEAK if number % rs.factor == 0)
-        or f"{number}"
-    )
+    return str(RAINSPEAK[number % RAINSPEAK == 0] or number)
