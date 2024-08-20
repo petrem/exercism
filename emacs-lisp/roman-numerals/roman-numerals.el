f64a2ba507253726cb0e55a4c43a09ce541c13ce
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-08-20 08:30:57 +0300

    emacs-list:roman-numerals:initial checkout

---
+++ emacs-lisp/roman-numerals/roman-numerals.el
@@ -0,0 +1,13 @@
+;;; roman-numerals.el --- roman-numerals Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun to-roman (value)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'roman-numerals)
+;;; roman-numerals.el ends here
