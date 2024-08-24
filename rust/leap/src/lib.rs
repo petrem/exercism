d7eb1b33dc06f0bcdcbbc43eccaed2ddc454085f
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-08-24 16:23:02 +0300

    exercism:rust:leap

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn is_leap_year(year: u64) -> bool {
+    //TODO: are there partials to make an "is_year_divisible_by"?
+    return is_divisible_by(year, 4) && (!is_divisible_by(year, 100) || is_divisible_by(year, 400));
+}
+
+//TODO: have this work with integers in general
+fn is_divisible_by(dividend: u64, divisor: u64) -> bool {
+    return dividend % divisor == 0;
+}
