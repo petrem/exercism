2b2b6d91269dcca1fc836fad30b75ec564c69d8f
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-08-30 22:00:09 +0300

    exercism:emacs-lisp:roman-numerals

--- emacs-lisp/roman-numerals/roman-numerals.el
+++ emacs-lisp/roman-numerals/roman-numerals.el
@@ -4,30 +4,37 @@
 
 ;;; Code:
 
+(defun to-roman (value)
+  "Convert VALUE to roman numerals string."
+  (seq-reduce
+   (lambda (acc elem) (concat acc (roman-numerals--digits-for value (car elem) (cdr elem))))
+   roman-numerals--digits
+   ""))
+
+
 (defmacro roman-numerals--make-digits (one five ten)
   "Make a vector of roman digits from zero to ten.
 It uses ONE, FIVE and TEN to infer the other digits, e.g.
 \(roman-numerals--make-digits ?X ?L ?C)
-    => [\"\" \"X\" \"XX\" ... \"XC\" \"C\"]"
+    => [\"\" \"X\" \"XX\" ... \"XC\" \"C\"]
+
+Note to self: I'm keeping this as is for <3 reasons as my first macro."
   `(vconcat (mapcar 'concat
                     '(() (,one) (,one ,one) (,one ,one ,one)
                       (,one ,five) (,five) (,five ,one) (,five ,one ,one)
                       (,five ,one ,one ,one) (,one ,ten) (,ten)))))
 
 (defconst roman-numerals--digits
-  (vector (roman-numerals--make-digits ?I ?V ?X)  ;; 0: I..X
-          (roman-numerals--make-digits ?X ?L ?C)  ;; 1: X..C
-          (roman-numerals--make-digits ?C ?D ?M)  ;; 2: C..M
-          ["" "M" "MM" "MMM"])                     ;; 3: M..MMM
+  `((3 . ["" "M" "MM" "MMM"])
+    (2 . ,(roman-numerals--make-digits ?C ?D ?M))
+    (1 . ,(roman-numerals--make-digits ?X ?L ?C))
+    (0 . ,(roman-numerals--make-digits ?I ?V ?X)))
   "Roman digits from 0 to 10 for singles, tens, hundreds and thousands.")
 
-(defun to-roman (value)
-  "Convert VALUE to roman numerals string."
-  (concat (aref (aref roman-numerals--digits 3) (/ value 1000))
-          (aref (aref roman-numerals--digits 2) (/ (% value 1000) 100))
-          (aref (aref roman-numerals--digits 1) (/ (% value 100) 10))
-          (aref (aref roman-numerals--digits 0) (% value 10))))
 
+(defun roman-numerals--digits-for (number position roman-digits)
+  "Choose from ROMAN-DIGITS the one corresponding to NUMBER at POSITION."
+  (aref roman-digits (/ (% number (expt 10 (1+ position))) (expt 10 position))))
 
 (provide 'roman-numerals)
 ;;; roman-numerals.el ends here
