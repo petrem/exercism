6bbf5ca3d5893593766179864c302176884834ea
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-03 15:47:40 +0300

    exercism:rust:reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,14 +1,7 @@
 pub fn reverse(input: &str) -> String {
-    fn rev(input: &str, mut acc: String) -> String {
-        let mut input_chars = input.chars();
-        match input_chars.next() {
-            None => acc,
-            Some(head) => {
-                acc.insert(0, head);
-                rev(input_chars.as_str(), acc)
-            }
-        }
+    let mut reversed = String::new();
+    for chr in input.chars().rev() {
+        reversed.push(chr);
     }
-
-    rev(input, String::new())
+    reversed
 }
