7240b852c95f880d02ad3f349e4eb0287e52d0da
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-04 11:51:47 +0300

    exercism:rust:raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,58 +1,64 @@
-use std::fmt;
+mod internal {
+    use std::fmt;
 
-enum Sound {
-    Pling,
-    Plang,
-    Plong,
-}
+    enum Sound {
+        Pling,
+        Plang,
+        Plong,
+    }
 
-impl fmt::Display for Sound {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Pling => write!(f, "Pling"),
-            Plang => write!(f, "Plang"),
-            Plong => write!(f, "Plong"),
+    impl fmt::Display for Sound {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                Pling => write!(f, "Pling"),
+                Plang => write!(f, "Plang"),
+                Plong => write!(f, "Plong"),
+            }
         }
     }
-}
 
-struct RainWord {
-    modulus: u32,
-    word: Sound,
-}
+    struct RainWord {
+        modulus: u32,
+        word: Sound,
+    }
 
-impl RainWord {
-    fn to_string_from(&self, number: u32) -> String {
-        if number % self.modulus == 0 {
-            self.word.to_string()
+    impl RainWord {
+        fn to_string_from(&self, number: u32) -> String {
+            if number % self.modulus == 0 {
+                self.word.to_string()
+            } else {
+                String::new()
+            }
+        }
+    }
+
+    use Sound::*;
+
+    const RAIN_SPEAK: [RainWord; 3] = [
+        RainWord {
+            modulus: 3,
+            word: Pling,
+        },
+        RainWord {
+            modulus: 5,
+            word: Plang,
+        },
+        RainWord {
+            modulus: 7,
+            word: Plong,
+        },
+    ];
+
+    pub fn raindrops(n: u32) -> String {
+        let word: String = RAIN_SPEAK.iter().map(|x| x.to_string_from(n)).collect();
+        if word.is_empty() {
+            n.to_string()
         } else {
-            String::new()
+            word
         }
     }
 }
-
-use Sound::*;
-
-const RAIN_SPEAK: [RainWord; 3] = [
-    RainWord {
-        modulus: 3,
-        word: Pling,
-    },
-    RainWord {
-        modulus: 5,
-        word: Plang,
-    },
-    RainWord {
-        modulus: 7,
-        word: Plong,
-    },
-];
 
 pub fn raindrops(n: u32) -> String {
-    let word: String = RAIN_SPEAK.iter().map(|x| x.to_string_from(n)).collect();
-    if word.is_empty() {
-        n.to_string()
-    } else {
-        word
-    }
+    internal::raindrops(n)
 }
