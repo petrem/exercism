859f688dee658f8d66edf29aea7992484200e721
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-16 20:14:01 +0300

    exercism:rust:sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,36 +1,80 @@
-use std::iter::repeat;
+//This version is very inefficient computation-wise, but at least not a memory hog.
+
+struct Sieve {
+    primes: Vec<u64>,
+}
+
+impl Sieve {
+    fn new() -> Sieve {
+        Sieve { primes: vec![] }
+    }
+}
+
+impl Iterator for Sieve {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let next = match self.primes.last() {
+            None => 2,
+            Some(2) => 3, // so that we can add two for all the following candidates
+            Some(last) => (last + 2..)
+                .step_by(2)
+                .find(|&candidate| {
+                    self.primes
+                        .iter()
+                        .all(|&prime| !is_multiple_of(candidate, prime))
+                })
+                .expect("There are no more primes, aliens invading?!."),
+        };
+        self.primes.push(next);
+        Some(next)
+    }
+}
+
+fn is_multiple_of(n: u64, m: u64) -> bool {
+    (m..=n).step_by(m as usize).any(|x| x == n)
+}
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut markers: Vec<bool> = repeat(true).take((upper_bound + 1) as usize).collect();
-    markers[0] = false;
-    markers[1] = false;
-
-    let check_bound = (upper_bound as f64).sqrt() as usize;
-
-    for i in 2..=check_bound {
-        if markers[i] {
-            for j in (i * i..=(upper_bound as usize)).step_by(i) {
-                markers[j] = false;
-            }
-        }
-    }
-    markers
-        .into_iter()
-        .enumerate()
-        .filter(|(_, b)| *b)
-        .map(|(k, _)| k as u64)
+    Sieve::new()
+        .take_while(|&prime| prime <= upper_bound)
         .collect()
 }
 
-// exercism will run ignored tests, apparently -- so it times out
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     #[ignore]
-//     fn find_primes_up_to_100_000_000() {
-//         let output = primes_up_to(100_000_000);
-//         assert_eq!(output.len(), 5761455); // from https://en.wikipedia.org/wiki/Prime_number_theorem#Table_of_%CF%80(x),_x_/_log_x,_and_li(x)
-//     }
-// }
+    #[test]
+    fn nine_is_multiple_of_three() {
+        assert!(is_multiple_of(9, 3));
+    }
+
+    #[test]
+    fn a_hunderd_twenty_five_is_multiple_of_five() {
+        assert!(is_multiple_of(125, 5));
+    }
+
+    #[test]
+    fn eight_is_not_multiple_of_three() {
+        assert!(!is_multiple_of(8, 3));
+    }
+
+    #[test]
+    fn ten_is_not_multiple_of_three() {
+        assert!(!is_multiple_of(10, 3));
+    }
+
+    #[test]
+    fn nine_is_not_multiple_of_four() {
+        assert!(!is_multiple_of(9, 4));
+    }
+
+    // Exercism will run ignored tests, apparently. Commenting out to avoid a timeout.
+    // #[test]
+    // #[ignore]
+    // fn find_primes_up_to_100_000_000() {
+    //     let output = primes_up_to(100_000_000);
+    //     assert_eq!(output.len(), 5761455); // from https://en.wikipedia.org/wiki/Prime_number_theorem#Table_of_%CF%80(x),_x_/_log_x,_and_li(x)
+    // }
+}
