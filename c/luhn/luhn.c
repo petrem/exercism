87b0867bb8e44249ecb0c6a94ce4823fd701dba6
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-18 13:35:31 +0300

    exercism:c:luhn

--- c/luhn/luhn.c
+++ c/luhn/luhn.c
@@ -2,12 +2,12 @@
 #include "luhn.h"
 
 bool luhn(const char *num) {
-  int n_digits = 0, sum = 0;
+  int n_digits = 0, sum = 0, luhn_double[] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
   
   for (const char *p = num + strlen(num) - 1; p >= num; p--) {
     if (*p == ' ') continue;
     if (*p < '0' || *p > '9') return false;
-    sum += ((*p - '0') << (n_digits & 0x01)) > 9 ? ((*p - '0') << (n_digits++ & 0x01)) - 9 : (*p - '0') << (n_digits++ & 0x01);
+    sum += n_digits++ & 0x01 ? luhn_double[*p - '0'] : *p - '0';
   }
   
   return n_digits >= 2 && sum % 10 == 0;
