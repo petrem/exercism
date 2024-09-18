1d9c1738fc9c8e321ce056b8d046fc7f25fac9de
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-18 13:14:05 +0300

    exercism:c:luhn

--- c/luhn/luhn.c
+++ c/luhn/luhn.c
@@ -1,20 +1,16 @@
 #include <ctype.h>
-#include <stdbool.h>
 #include <string.h>
 
 #include "luhn.h"
 
 bool luhn(const char *num) {
-  int shift = 0, n_digits = 0, sum = 0;
-  for (const char *p = num + strlen(num) - 1; p >= num; p--) {
+  int n_digits = 0, sum = 0;
+  
+  for (const char *p = num + strlen(num) - 1; p >= num; p--)
     if (isdigit(*p)) {
-      int luhn_doubled = (*p - '0') << shift;
+      int luhn_doubled = (*p - '0') << (n_digits++ & 0x01);
       sum += luhn_doubled > 9 ? luhn_doubled - 9 : luhn_doubled;
-      shift ^= 0x01;
-      n_digits++;
-    } else if (*p != ' ') {
-      return false;
-    }
-  }
+    } else if (*p != ' ') return false;
+  
   return n_digits >= 2 && sum % 10 == 0;
 }
