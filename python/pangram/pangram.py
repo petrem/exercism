b1a64a798cd1d9107e21274faf04313cc142a67b
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-19 08:38:23 +0300

    exercism:python:pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,15 +1,5 @@
 import string
 
 
-alphabet = set(string.ascii_lowercase)
-
-
-def is_pangram(sentence):
-    letters = {letter.lower() for letter in sentence if letter.lower() in alphabet}
-    return letters == alphabet
-
-
-# the obligatory standard solution
-def is_pangram2(sentence):
-    lower_sentence = sentence.lower()
-    return all(letter in lower_sentence for letter in alphabet)
+def is_pangram(sentence: str) -> bool:
+    return set(string.ascii_lowercase).issubset(sentence.lower())
