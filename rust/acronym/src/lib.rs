d7dadd3ec98d00b8dc4d8cfd716265c9c8cb11bc
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-20 23:58:10 +0300

    rust::acronym::wip

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -2,11 +2,13 @@
 // with a pipeline.
 
 pub fn abbreviate(phrase: &str) -> String {
-    let mut fsm = AbbrFSM::new();
     phrase
         .chars()
-        .filter_map(|c| fsm.transition(c).map(|c| c.to_uppercase().to_string()))
-        .collect()
+        .fold((AbbrState::default(), std::iter::empty()), |(state, acc), c| -> {
+            let (newstate, emmitted) = state.transition(c);
+            (newstate, emmitted.and
+        // .filter_map(|c| fsm.transition(c).map(|c| c.to_uppercase().to_string()))
+        // .collect()
 }
 
 enum CharKind {
@@ -15,12 +17,8 @@ enum CharKind {
     Other,
 }
 
-impl CharKind {
-    // TODO: Is it idiomatic to put functions without self param and not returning Self
-    // into an impl? Do these have a name, like "static method" in Python?
-    // Would it be more typical to rather create a module encapsulating the
-    // struct and associated functions?
-    fn classify(c: char) -> CharKind {
+impl From<char> for CharKind {
+    fn from(c: char) -> CharKind {
         if c.is_uppercase() {
             CharKind::Upper
         } else if c.is_alphabetic() || c == '\'' {
@@ -31,47 +29,27 @@ impl CharKind {
     }
 }
 
-enum AbbrStates {
+enum AbbrState {
     SeekWordStart,
     InWordLower,
     InWordUpper,
 }
-use AbbrStates::*;
 
-struct AbbrFSM {
-    state: AbbrStates,
+use AbbrState::*;
+
+impl AbbrState {
+    fn transition(self, c: char) -> (Self, Option<char>) {
+        match (self, c.into()) {
+            (SeekWordStart, CharKind::Upper) => (InWordUpper, Some(c)),
+            (SeekWordStart, CharKind::Word) => (InWordLower, Some(c)),
+            (InWordUpper | InWordLower, CharKind::Other) => (SeekWordStart, None),
+            (InWordUpper, CharKind::Word) => (InWordLower, None),
+            (InWordLower, CharKind::Upper) => (InWordUpper, Some(c)),
+            (state, _) => (state, None),
+        }
+    }
 }
 
-impl AbbrFSM {
-    fn new() -> Self {
-        Self {
-            state: SeekWordStart,
-        }
-    }
-
-    fn transition(&mut self, c: char) -> Option<char> {
-        match (&self.state, CharKind::classify(c)) {
-            (SeekWordStart, CharKind::Upper) => {
-                self.state = InWordUpper;
-                Some(c)
-            }
-            (SeekWordStart, CharKind::Word) => {
-                self.state = InWordLower;
-                Some(c)
-            }
-            (InWordUpper | InWordLower, CharKind::Other) => {
-                self.state = SeekWordStart;
-                None
-            }
-            (InWordUpper, CharKind::Word) => {
-                self.state = InWordLower;
-                None
-            }
-            (InWordLower, CharKind::Upper) => {
-                self.state = InWordUpper;
-                Some(c)
-            }
-            _ => None,
-        }
-    }
+impl Default for AbbrState {
+    fn default() -> AbbrState { SeekWordStart }
 }
