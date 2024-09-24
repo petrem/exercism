53d8a38efee8a8f4c1fa3cdd1e470a92f9407a98
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-24 15:32:17 +0300

    exercism:python:isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,11 +1,5 @@
-from collections import Counter
-import string
-
-
-# Make a translation table to remove whitespace and hyphen
-TRANS_WSH = str.maketrans("", "", string.whitespace + "-")
+import re
 
 
 def is_isogram(string):
-    counted = Counter(string.lower().translate(TRANS_WSH))
-    return len(counted) == 0 or counted.most_common(1)[0][1] == 1
+    return re.search(r"([a-z]).*\1", string, re.IGNORECASE) is None
