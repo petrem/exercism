d8f1fd5df2a4d851d0a210c731cacb252b01c843
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-24 16:23:58 +0300

    exercism:python:yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,50 +1,22 @@
 from collections import Counter
-from functools import partial
 
 
 def _n_of_a_kind(n, op, dice):
     which, how_many = Counter(dice).most_common(1)[0]
-    if how_many >= n:
-        return op(which)
-    return 0
-
-
-YACHT = partial(_n_of_a_kind, 5, lambda _: 50)
-
-
-def _score_n(n, dice):
-    return n * Counter(dice).get(n, 0)
-
-
-ONES = partial(_score_n, 1)
-TWOS = partial(_score_n, 2)
-THREES = partial(_score_n, 3)
-FOURS = partial(_score_n, 4)
-FIVES = partial(_score_n, 5)
-SIXES = partial(_score_n, 6)
-
-
-def FULL_HOUSE(dice):
-    if sorted(Counter(dice).values()) == [2, 3]:
-        return sum(dice)
-    return 0
-
-
-FOUR_OF_A_KIND = partial(_n_of_a_kind, 4, lambda which: 4 * which)
+    return op(which) if how_many >= n else 0
 
 
 def _straight(start, dice):
-    if sorted(dice) == list(range(start, start+5)):
-        return 30
-    return 0
+    return 30 if sorted(dice) == list(range(start, start+5)) else 0
 
 
-LITTLE_STRAIGHT = partial(_straight, 1)
-BIG_STRAIGHT = partial(_straight, 2)
-
-
-def CHOICE(dice):
-    return sum(dice)
+ONES, TWOS, THREES, FOURS, FIVES, SIXES = (lambda dice, n=k: n * Counter(dice).get(n, 0) for k in range(1, 7))
+YACHT = lambda dice: _n_of_a_kind(5, lambda _: 50, dice)
+FOUR_OF_A_KIND = lambda dice: _n_of_a_kind(4, lambda which: 4 * which, dice)
+LITTLE_STRAIGHT = lambda dice: _straight(1, dice)
+BIG_STRAIGHT = lambda dice: _straight(2, dice)
+CHOICE = sum
+FULL_HOUSE = lambda dice: sum(dice) if sorted(Counter(dice).values()) == [2, 3] else 0
 
 
 def score(dice, category):
