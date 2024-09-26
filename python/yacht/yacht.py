d062d8782e16f5188aeb8b1ac8ff4e669e4c856f
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-26 14:01:40 +0300

    exercism:python:yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -6,15 +6,10 @@ def _n_of_a_kind(n, op, dice):
     return op(which) if how_many >= n else 0
 
 
-def _straight(start, dice):
-    return 30 if sorted(dice) == list(range(start, start+5)) else 0
-
-
-ONES, TWOS, THREES, FOURS, FIVES, SIXES = (lambda dice, n=k: n * Counter(dice).get(n, 0) for k in range(1, 7))
+ONES, TWOS, THREES, FOURS, FIVES, SIXES = (lambda dice, n=k: n * dice.count(n) for k in range(1, 7))
 YACHT = lambda dice: _n_of_a_kind(5, lambda _: 50, dice)
 FOUR_OF_A_KIND = lambda dice: _n_of_a_kind(4, lambda which: 4 * which, dice)
-LITTLE_STRAIGHT = lambda dice: _straight(1, dice)
-BIG_STRAIGHT = lambda dice: _straight(2, dice)
+LITTLE_STRAIGHT, BIG_STRAIGHT = (lambda dice, n=k: 30 if sorted(dice) == list(range(n, n+5)) else 0 for k in range(1, 3))
 CHOICE = sum
 FULL_HOUSE = lambda dice: sum(dice) if sorted(Counter(dice).values()) == [2, 3] else 0
 
