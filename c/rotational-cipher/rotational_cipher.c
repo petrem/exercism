e6d7f90866c6d50cac240865ed2ff18c8e2c0f8c
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-09-30 11:25:44 +0300

    exercism:c:rotational-cipher

--- c/rotational-cipher/rotational_cipher.c
+++ c/rotational-cipher/rotational_cipher.c
@@ -1,14 +1,11 @@
 #include "rotational_cipher.h"
 
 #include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
+
+char *strdup(const char *s);
 
 char *rotate(const char *text, int shift_key) {
-  char *rotated;
-  // would love to use strnlen() instead, but that's not conforming to C99
-  if (!text || !(rotated = (char *)malloc(strlen(text) + 1))) return NULL;
-  char *p = rotated;
+  char *rotated = strdup(text), *p = rotated;
   while ((*p++ = isupper(*text) ? (*text++ + shift_key - 13) % 26 + 'A' : islower(*text) ? (*text++ + shift_key - 19) % 26 + 'a' : *text++));
   return rotated;
 }
