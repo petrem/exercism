8f48bcf267f31b6e5f1f799264ef08106a9c3163
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-01 13:20:34 +0300

    exercism:rust:bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,30 +1,22 @@
-// Obviously, the disadvantage of this approach is that checks may be run when not needed.
-
 pub fn reply(message: &str) -> &str {
     Response::from(message).0
-}
-
-fn is_silence(phrase: &str) -> bool {
-    phrase.chars().all(|c| c.is_whitespace())
-}
-
-fn is_question(phrase: &str) -> bool {
-    phrase.trim_end().ends_with('?')
-}
-
-fn is_shout(phrase: &str) -> bool {
-    phrase.contains(|c: char| c.is_uppercase()) && !phrase.contains(|c: char| c.is_lowercase())
 }
 
 struct Response(&'static str);
 
 impl From<&str> for Response {
     fn from(msg: &str) -> Self {
-        match (is_silence(msg), is_question(msg), is_shout(msg)) {
-            (true, _, _) => Response("Fine. Be that way!"),
-            (false, true, true) => Response("Calm down, I know what I'm doing!"),
-            (false, false, true) => Response("Whoa, chill out!"),
-            (false, true, false) => Response("Sure."),
+        let msg = msg.trim_end();
+        if msg.is_empty() {
+            return Response("Fine. Be that way!");
+        }
+        let is_question = msg.ends_with('?');
+        let is_shout =
+            msg.contains(|c: char| c.is_uppercase()) && !msg.contains(|c: char| c.is_lowercase());
+        match (is_question, is_shout) {
+            (true, true) => Response("Calm down, I know what I'm doing!"),
+            (false, true) => Response("Whoa, chill out!"),
+            (true, false) => Response("Sure."),
             _ => Response("Whatever."),
         }
     }
