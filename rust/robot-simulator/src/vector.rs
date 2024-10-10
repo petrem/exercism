e13193a33c346e075d1f9bf591fc6c6ffa4324b7
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-10 13:43:44 +0300

    exercism:rust:robot-simulator

--- rust/robot-simulator/src/vector.rs
+++
@@ -1,6 +0,0 @@
-//! Simple, two-dimensional vectors.
-//! The term "arrow" is vague, but hopefully less misleading then Vector,
-//! which could be confused both for a `Vec` or to a general, multi-dimensional vector.
-
-struct Arrow {
-}
