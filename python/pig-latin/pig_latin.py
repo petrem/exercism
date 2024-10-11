5908efd336edc45c9c422cef1fcd93ddcf2804b7
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-11 10:52:50 +0300

    exercism:python:pig-latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,5 +1,7 @@
 """Translate Human English into Piglet English."""
 
+# ruff: noqa: ERA001
+
 
 def translate(text):
     """Rewrite words in ``text`` into Piglish."""
@@ -7,24 +9,27 @@ def translate(text):
     return " ".join(_igmepay(word) for word in text.split())
 
 
-_VOWELS = frozenset("aeiou")
-_VOWELS_PLUS = frozenset("aeiouy")
+_VOWELS_START = (*tuple("aeiou"), "xr", "yt")
+_VOWELS_MIDDLE = frozenset("aeiouy")
 
 
 def _igmepay(word: str) -> str:
     """Rewrite a word as Piglish."""
-    # Faster, albeit more verbose, than e.g. ``word[:2] in {"xr", "yt"}``
-    # or even ``word.startswith(('a', .., 'xr', 'yt')``
-    if any(
-        (
-            (length := len(word)) == 1,
-            (first := word[0]) in _VOWELS,
-            first == "x" and word[1] == "r",
-            first == "y" and word[1] == "t",
-        )
-    ):
+    # The following seemd faster last time I looked at this exercise, but
+    # this time (py3.12) it is slower.
+    # if any(
+    #     (
+    #         (length := len(word)) == 1,
+    #         (first := word[0]) in _VOWELS,
+    #         first == "x" and word[1] == "r",
+    #         first == "y" and word[1] == "t",
+    #     )
+    # ):
+    if word.startswith(_VOWELS_START):
         return word + "ay"
-    split_at = next((i for i in range(1, length) if word[i] in _VOWELS_PLUS), length)
+    length = len(word)
+    split_at = next((i for i in range(1, length) if word[i] in _VOWELS_MIDDLE), length)
+    # This is still faster then e.g word.find(..) or slicing
     if split_at != length and word[split_at] == "u" and word[split_at - 1] == "q":
         # include "qu" in consonants group
         split_at += 1
