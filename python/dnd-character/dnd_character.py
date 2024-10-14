9a436ff77fd65b2bf69ce2f4cbf7fd9001269842
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-14 18:41:42 +0300

    exercism:python:dnd-character

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -7,13 +7,19 @@ def modifier(c):
 
 class Character:
     ABILITIES = (
-        "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"
+        "strength",
+        "dexterity",
+        "constitution",
+        "intelligence",
+        "wisdom",
+        "charisma",
     )
 
     def __init__(self):
         for attr in self.ABILITIES:
             setattr(self, attr, self.ability())
-        setattr(self, "hitpoints", 10 + modifier(self.constitution))
+        self.hitpoints = 10 + modifier(self.constitution)
 
     def ability(self):
+        # ruff: noqa: S311
         return sum(sorted(random.choices(range(1, 7), k=4))[1:])
