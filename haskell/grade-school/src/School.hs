4456b976e6db3b14099b3d6d8a36085cf21dc56b
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-15 18:01:17 +0300

    exercism:haskell:grade-school

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,22 +1,23 @@
 module School (School, add, empty, grade, sorted) where
 
-import qualified Data.List as L
 import Data.IntMap (IntMap)
 import qualified Data.IntMap.Strict as IntMap
-
+import Data.List (insert)
+import Data.Maybe (fromMaybe)
 
 type School = IntMap [String]
 
 add :: Int -> String -> School -> School
 add gradeNum student = IntMap.alter upsert gradeNum
-  where upsert Nothing         = Just [student]
-        upsert (Just existing) = Just (L.insert student existing)
+  where
+    upsert Nothing = Just [student]
+    upsert (Just existing) = Just (insert student existing)
 
 empty :: School
 empty = IntMap.empty
 
 grade :: Int -> School -> [String]
-grade gradeVal = (maybe [] id) . (IntMap.lookup gradeVal)
+grade gradeVal = fromMaybe [] . IntMap.lookup gradeVal
 
 sorted :: School -> [(Int, [String])]
 sorted = IntMap.assocs
