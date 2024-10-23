65ca6b3096de400c3336634a54aeafa35d2072d2
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-23 18:56:57 +0300

    exercism:erlang:parallel-letter-frequency

--- erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
+++ erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
@@ -1,20 +1,28 @@
 -module(parallel_letter_frequency).
 
--export([dict/1, letter_frequencies/1]).
-
-%% first, a non-parallel solution (and a few more tests)
+-export([dict/1]).
 
 
-dict(_Strings) ->
-    lists:foldr( fun(_S, _Acc) ->
-                         dict:merge(fun(_, _V1, _V2) -> _V1 + _V2 end
-                                   , letter_frequencies(_S)
-                                   , _Acc
-                                   )
-                 end
-               , dict:new()
-               , _Strings
-             ).
+dict(Strings) ->
+    Workers = [start_worker(String) || String <- Strings],
+    lists:foldl(fun(Pid, Acc) ->
+                        Freqs = gather_worker(Pid),
+                        dict:merge(fun(_K, V1, V2) ->
+                                           V1 + V2 end, Freqs, Acc)
+                end,
+                dict:new(),
+                Workers).
+
+start_worker(String) ->
+    Parent = self(),
+    spawn_link(fun() ->
+                       Parent ! {self(), {reply, letter_frequencies(String)}}
+               end).
+
+gather_worker(Pid) ->
+    receive {Pid, {reply, Result}} -> Result end.
+                                  
+    
 
 letter_frequencies(_String) ->
     lists:foldr( fun(_K, _Acc) -> dict:update_counter(_K, 1, _Acc) end
