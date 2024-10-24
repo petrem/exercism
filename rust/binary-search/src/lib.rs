552c4e269ebc0ec62666231d413d770457d193d1
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-24 09:19:40 +0300

    exercism:rust:binary-search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,12 +1,14 @@
+use std::cmp::Ordering;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
     let mut haystack = array;
     let mut offset: usize = 0;
     while !haystack.is_empty() {
         let middle: usize = haystack.len() / 2;
-        haystack = match haystack[middle] {
-            v if v == key => return Some(offset + middle),
-            v if key < v => &haystack[..middle],
-            _ => {
+        haystack = match haystack[middle].cmp(&key) {
+            Ordering::Equal => return Some(offset + middle),
+            Ordering::Greater => &haystack[..middle],
+            Ordering::Less => {
                 offset += middle + 1;
                 &haystack[middle + 1..]
             }
