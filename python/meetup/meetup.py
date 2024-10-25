bd5f0a4ab0ae756af517d8f70cf7275f72a00fd2
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-25 15:19:02 +0300

    exercism:python:meetup

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,13 +1,10 @@
 import calendar
-from datetime import date, timedelta
+from datetime import date
 from functools import partialmethod
 
 
 class MeetupDayException(Exception):
     pass
-
-
-WEEK = timedelta(days=7)
 
 
 def meetup(year, month, which, day_of_the_week):
@@ -17,30 +14,28 @@ def meetup(year, month, which, day_of_the_week):
 
 class DateFinder:
     def __init__(self, which):
-        self.strategy = getattr(self, which)  # n.b. this is fragile
+        self.get_base = getattr(self, which)  # n.b. this is fragile
 
     def __call__(self, year, month, day_of_week):
-        return self.strategy(getattr(calendar, day_of_week.upper()), year, month)
+        day_of_week = getattr(calendar, day_of_week.upper())
+        base = self.get_base(year, month)
+        try:
+            base_weekday = date(year, month, base).weekday()
+            return date(year, month, base + (7 + day_of_week - base_weekday) % 7)
+        except ValueError as e:
+            raise MeetupDayException("That day does not exist.") from e
 
-    def teenth(self, day_of_week, year, month):
-        return self.days_until(date(year, month, 13), day_of_week)
+    def teenth(self, _year, _month):
+        return 13
 
-    def last(self, day_of_week, year, month):
-        d = self.days_until(date(year, month, 1), day_of_week) + 4 * WEEK
-        return d if d.month == month else d - WEEK
+    def last(self, year, month):
+        return calendar.monthrange(year, month)[1] - 6
 
-    def nth(self, nth, day_of_week, year, month):
-        d = self.days_until(date(year, month, 1), day_of_week) + nth * WEEK
-        if d.month != month:
-            raise MeetupDayException("That day does not exist.")
-        return d
+    def nth(self, nth, _year, _month):
+        return nth * 7 + 1
 
     first = partialmethod(nth, 0)
     second = partialmethod(nth, 1)
     third = partialmethod(nth, 2)
     fourth = partialmethod(nth, 3)
     fifth = partialmethod(nth, 4)
-
-    @staticmethod
-    def days_until(from_date, day_of_week):
-        return from_date + timedelta(days=(7 + day_of_week - from_date.weekday()) % 7)
