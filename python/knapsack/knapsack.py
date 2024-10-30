ab64f4f693938f4425660d8d9f41de7cc4999eb8
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-10-30 17:56:59 +0200

    exercism:python:knapsack

--- python/knapsack/knapsack.py
+++ python/knapsack/knapsack.py
@@ -1,12 +1,6 @@
-# Obligatory ;-) exhaustive solution
-from functools import reduce
-from itertools import combinations
-
-
-def maximum_value(maximum_weight, items):
-    return max(
-        (totals[1]
-         for j in range(1, len(items))
-         for c in combinations(items, j)
-         if (totals := reduce(lambda acc, i: (i["weight"] + acc[0], i["value"] + acc[1]), c, (0, 0)))[0] <= maximum_weight),
-        default=0)
+# simple dynamic programming solution
+def maximum_value(W, items):
+    row = [0] * (W+1)
+    for item in items:
+        row = [*[row[w] for w in range(0, min(item["weight"], W+1))], *[max(row[w], row[w-item["weight"]]+item["value"]) for w in range( min(item["weight"], W+1), W+1)]]
+    return row[-1]
