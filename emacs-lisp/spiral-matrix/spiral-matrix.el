77da666ce717f175458fd5b6e5f24f237e665c9e
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-02 14:46:11 +0200

    exercism:emacs-lisp:spiral-matrix

---
+++ emacs-lisp/spiral-matrix/spiral-matrix.el
@@ -0,0 +1,28 @@
+;;; spiral-matrix.el --- Spiral Matrix (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun spiral-matrix (size &optional from)
+  (let ((from (or from 1)))
+    (pcase size
+      (0 [])
+      (1 `[[,from]])
+      (_ (let* ((top-right    (+ from         size -1))
+                (bottom-right (+ top-right    size -1))
+                (bottom-left  (+ bottom-right size -1))
+                (top-left     (+ bottom-left  size -1))
+                (top          (seq-into (number-sequence from           top-right          ) 'vector))
+                (bottom       (seq-into (number-sequence bottom-left    bottom-right     -1) 'vector))
+                (left         (seq-into (number-sequence (1- top-left)  (1+ bottom-left) -1) 'vector))
+                (right        (seq-into (number-sequence (1+ top-right) (1- bottom-right)  ) 'vector))
+                (inner        (spiral-matrix (- size 2) top-left))
+                (middle       (seq-into (seq-mapn #'vpad left inner right) 'vector)))
+           (vpad top middle bottom))))))
+
+(defsubst vpad (l m r) (vconcat `[,l] m `[,r]))
+
+(provide 'spiral-matrix)
+;;; spiral-matrix.el ends here
