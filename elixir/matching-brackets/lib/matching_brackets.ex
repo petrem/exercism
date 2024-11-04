33b6f7c1a032b682f8691a14b47c9f8189c9cfdc
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-04 13:42:22 +0200

    exercism:elixir:matching-brackets

--- elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -3,17 +3,17 @@ defmodule MatchingBrackets do
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t()) :: boolean
-  def check_brackets(str), do: to_charlist(str) |> Enum.filter(&(Enum.member?([?{,?},?[,?],?(,?)], &1))) |> is_paired([])
+  def check_brackets(str), do: to_charlist(str) |> Enum.filter(&(Enum.member?([?{,?},?[,?],?(,?)], &1))) |> paired?([])
 
-  defp is_paired([], []), do: true
-  defp is_paired([], [_|_]), do: false
-  defp is_paired([c | rest], stack) when c == ?{ or c == ?[  or c == ?(, do: is_paired(rest, [c|stack])
-  defp is_paired([c | rest], [top|stack]) do
+  defp paired?([], []), do: true
+  defp paired?([], [_|_]), do: false
+  defp paired?([c | rest], stack) when c == ?{ or c == ?[  or c == ?(, do: paired?(rest, [c|stack])
+  defp paired?([c | rest], [top|stack]) do
     if %{?{ => ?}, ?[ => ?], ?( => ?)}[top] == c do
-      is_paired(rest, stack)
+      paired?(rest, stack)
     else
       false
     end
   end
-  defp is_paired(_, []), do: false
+  defp paired?(_, []), do: false
 end
