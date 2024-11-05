b57c3c9f197ff55c1dd48283f70bb7c5222084aa
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-05 16:29:22 +0200

    exercism:rust:eliuds-eggs

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn egg_count(display_value: u32) -> usize {
-    let mut count: usize = 0;
-    let mut eggs = display_value as usize;
-    while eggs > 0 {
-        count += eggs & 1;
-        eggs >>= 1;
-    }
-    count
+    std::iter::successors(Some(display_value), |n| {
+        (*n > 0).then_some(std::ops::Shr::shr(n, 1))
+    })
+    .filter(|n| n & 1 == 1)
+    .count()
 }
