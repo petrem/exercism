336c9d18caeb8d9b41c26778aeab094c3e02f575
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-06 18:42:30 +0200

    exercism:emacs-lisp:food-chain

--- emacs-lisp/food-chain/food-chain.el
+++ emacs-lisp/food-chain/food-chain.el
@@ -34,7 +34,7 @@
 (defsubst qualify (animal)
   (when (is-qualified animal) (concat " that " (cadr animal))))
 
-(defsubst justify (animal1 animal2)
+(defsubst explain (animal1 animal2)
   (concat "She swallowed the " (name animal1) " to catch the " (name animal2) (qualify animal2) "."))
 
 (defsubst take-until (testp seq)
@@ -44,13 +44,13 @@
 (defun make-stanza (index)
   (let* ((animal (aref all-animals index))
          (animals (take-until #'is-last (reverse (seq-take all-animals (1+ index)))))
-         (tuplets (seq-mapn #'justify animals (seq-rest animals))))
+         (accretion (seq-mapn #'explain  animals (seq-rest animals))))
     (nconc
      (list ""
            (concat "I know an old lady who swallowed a " (name animal) ".")
            (utter animal))
      (unless (is-last animal)
-       (nconc tuplets (list (utter (car (last animals)))))))))
+       (nconc accretion (list (utter (car (last animals)))))))))
 
 (provide 'food-chain)
 ;;; food-chain.el ends here
