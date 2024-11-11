3be3017487798a365ec49a9c48fd6848aba529b3
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-11 17:06:37 +0200

    exercism:rust:rna-transcription

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,93 +1,102 @@
-// let's start somewhere... will improve it later
+// Nucleotides
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Dna(Vec<DnaNucleotide>);
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum DnaNucleotide {
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Nucleotide {
     A,
     C,
     G,
     T,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Rna(Vec<RnaNucleotide>);
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum RnaNucleotide {
-    A,
-    C,
-    G,
     U,
 }
 
-impl TryFrom<char> for RnaNucleotide {
+#[derive(Debug, PartialEq, Eq)]
+pub struct Dna(Vec<Nucleotide>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Rna(Vec<Nucleotide>);
+
+impl Dna {
+    pub fn new(strand: &str) -> Result<Self, usize> {
+        <Self as Strand>::from_str(strand)
+            .collect::<Result<Vec<Nucleotide>, _>>()
+            .map(Dna)
+    }
+
+    pub fn into_rna(&self) -> Rna {
+        Rna::from_iter(self.0.iter().map(Self::translate))
+    }
+
+    fn translate(nucleotide: &Nucleotide) -> Nucleotide {
+        match nucleotide {
+            Nucleotide::A => Nucleotide::U,
+            Nucleotide::C => Nucleotide::G,
+            Nucleotide::G => Nucleotide::C,
+            Nucleotide::T => Nucleotide::A,
+            _ => panic!("Cannot translate U"),
+        }
+    }
+}
+
+impl Rna {
+    pub fn new(strand: &str) -> Result<Self, usize> {
+        <Self as Strand>::from_str(strand)
+            .collect::<Result<Vec<Nucleotide>, _>>()
+            .map(Rna)
+    }
+}
+
+impl FromIterator<Nucleotide> for Rna {
+    fn from_iter<I: IntoIterator<Item = Nucleotide>>(iter: I) -> Rna {
+        Rna(iter.into_iter().collect())
+    }
+}
+
+impl TryFrom<char> for Nucleotide {
     type Error = String;
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
         match value {
-            'A' => Ok(RnaNucleotide::A),
-            'C' => Ok(RnaNucleotide::C),
-            'G' => Ok(RnaNucleotide::G),
-            'U' => Ok(RnaNucleotide::U),
+            'A' => Ok(Nucleotide::A),
+            'C' => Ok(Nucleotide::C),
+            'G' => Ok(Nucleotide::G),
+            'T' => Ok(Nucleotide::T),
+            'U' => Ok(Nucleotide::U),
             _ => Err(format!("Cannot make RnaNucleotide from {value}")),
         }
     }
 }
 
-impl TryFrom<char> for DnaNucleotide {
-    type Error = String;
+trait Strand {
+    const VALID: [Nucleotide; 4];
+    fn valid(nucleotide: &Nucleotide) -> bool
+    where
+        Self: Sized,
+    {
+        <Self as Strand>::VALID.contains(nucleotide)
+    }
 
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            'A' => Ok(DnaNucleotide::A),
-            'C' => Ok(DnaNucleotide::C),
-            'G' => Ok(DnaNucleotide::G),
-            'T' => Ok(DnaNucleotide::T),
-            _ => Err(format!("Cannot make DnaNucleotide from {value}")),
-        }
+    fn from_str(strand: &str) -> Box<dyn Iterator<Item = Result<Nucleotide, usize>> + '_>
+    where
+        Self: Sized,
+    {
+        Box::new(strand.chars().enumerate().map(|(i, chr)| {
+            chr.try_into()
+                .map_err(|_| i)
+                .and_then(|nucleotide: Nucleotide| {
+                    if Self::valid(&nucleotide) {
+                        Ok(nucleotide)
+                    } else {
+                        Err(i)
+                    }
+                })
+        }))
     }
 }
 
-impl Dna {
-    pub fn new(dna: &str) -> Result<Dna, usize> {
-        dna.chars()
-            .enumerate()
-            .map(|(i, chr)| chr.try_into().map_err(|_| i))
-            .collect()
-    }
-
-    pub fn into_rna(self) -> Rna {
-        self.0
-            .iter()
-            .map(|nucleotide| match nucleotide {
-                DnaNucleotide::C => RnaNucleotide::G,
-                DnaNucleotide::G => RnaNucleotide::C,
-                DnaNucleotide::T => RnaNucleotide::A,
-                DnaNucleotide::A => RnaNucleotide::U,
-            })
-            .collect()
-    }
+impl Strand for Rna {
+    const VALID: [Nucleotide; 4] = [Nucleotide::A, Nucleotide::C, Nucleotide::G, Nucleotide::U];
 }
 
-impl Rna {
-    pub fn new(rna: &str) -> Result<Rna, usize> {
-        rna.chars()
-            .enumerate()
-            .map(|(i, chr)| chr.try_into().map_err(|_| i))
-            .collect()
-    }
-}
-
-impl FromIterator<RnaNucleotide> for Rna {
-    fn from_iter<T: IntoIterator<Item = RnaNucleotide>>(iter: T) -> Self {
-        Rna(iter.into_iter().collect())
-    }
-}
-
-impl FromIterator<DnaNucleotide> for Dna {
-    fn from_iter<T: IntoIterator<Item = DnaNucleotide>>(iter: T) -> Self {
-        Dna(iter.into_iter().collect())
-    }
+impl Strand for Dna {
+    const VALID: [Nucleotide; 4] = [Nucleotide::A, Nucleotide::C, Nucleotide::G, Nucleotide::T];
 }
