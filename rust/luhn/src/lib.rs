8e34d2574f8596d72b37b76be9df87b02dd2a13a
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-12 12:48:47 +0200

    exercism:rust:luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,17 +1,10 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if let Some(digits) = code
-        .chars()
-        .filter(|c| *c != ' ')
-        .map(|c| c.to_digit(10))
-        .rev()
-        .collect::<Option<Vec<_>>>()
-        .and_then(|ds| (ds.len() >= 2).then_some(ds)) {
-            let luhn_sum_odds: u32 = digits.iter().skip(1).step_by(2).map(|d| if d * 2 < 10 {d * 2} else {d * 2 - 9}).sum();
-            let luhn_sum_evens: u32 = digits.iter().step_by(2).sum();
-            (luhn_sum_odds + luhn_sum_evens) % 10 == 0
-        } else {
-            false
-        }
+    code.chars().filter(|c| *c != ' ').map(|c| c.to_digit(10)).rev()
+        .collect::<Option<Vec<_>>>().and_then(|ds| (ds.len() >= 2).then_some(ds))
+        .map(|ds| {
+            ds.iter().zip([false, true].iter().cycle()).fold(0, |acc, (d, &double)| {
+                acc + if double { [0, 2, 4, 6, 8, 1, 3, 5, 7, 9][*d as usize] } else { *d }
+            }) % 10 == 0
+        }).unwrap_or(false)
 }
-
