a86cd8a84f5cee570050a68da83c1d33ae36d497
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-12 19:18:37 +0200

    exercism:rust:nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -4,7 +4,8 @@ pub fn nth(n: u32) -> u32 {
         return primes[n as usize];
     }
     for i in (5..).step_by(2) {
-        if is_pairwise_coprime_with(i, &primes) {
+        let sqrt_i = (i as f32).sqrt() as usize;
+        if is_pairwise_coprime_with(i, &primes[..sqrt_i]) {
             primes.push(i);
             if primes.len() == (n + 1) as usize {
                 break;
