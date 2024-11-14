7fa9c095a9e7546171c7edd3b027edaa6c48d51c
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-14 16:04:07 +0200

    exercism:rust:proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,16 +1,19 @@
+use std::iter::once;
+
 pub fn build_proverb(words: &[&str]) -> String {
-    let mut proverb = words
-        .windows(2)
-        .map(|w| {
-            if let [w1, w2] = w {
-                format!("For want of a {w1} the {w2} was lost.\n")
-            } else {
-                panic!("I said windows of two!")
-            }
-        })
-        .collect::<String>();
     if let Some(first) = words.first() {
-        proverb.push_str(format!("And all for the want of a {}.", first).as_str());
+        words
+            .windows(2)
+            .map(|chunk| {
+                if let [w1, w2] = chunk {
+                    format!("For want of a {w1} the {w2} was lost.\n")
+                } else {
+                    panic!("Shouldn't happen, I said windows of two!")
+                }
+            })
+            .chain(once(format!("And all for the want of a {first}.")))
+            .collect()
+    } else {
+        String::new()
     }
-    proverb
 }
