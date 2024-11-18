bbac64e9bf3934cf9a1b5591b65e14b13c970a51
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-18 14:28:43 +0200

    exercism:c:list-ops

--- c/list-ops/list_ops.c
+++ c/list-ops/list_ops.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define REALLOC_FRACTION 0.75
+
 static inline list_t *realloc_list(list_t *list, size_t length);
 
 
-list_t *new_list(size_t length, list_value_t values[]) {
+list_t *new_list(size_t length, list_element_t elements[]) {
   list_t *list = realloc_list(NULL, length);
   if (list != NULL) {
     list->length = length;
-    memcpy(list->values, values, length * sizeof(list_value_t));
+    memcpy(list->elements, elements, length * sizeof(list_element_t));
   }
   return list;
 }
@@ -19,26 +22,27 @@ list_t *append_list(list_t *list1, list_t *list2) {
   list_t *list = realloc_list(NULL, list1->length + list2->length);
   if (list != NULL) {
     list->length = list1->length + list2->length;
-    memcpy(list->values,  list1->values, list1->length * sizeof(list_value_t));
-    memcpy(list->values + list1->length,
-           list2->values, list2->length * sizeof(list_value_t));
+    memcpy(list->elements,  list1->elements, list1->length * sizeof(list_element_t));
+    memcpy(list->elements + list1->length,
+           list2->elements, list2->length * sizeof(list_element_t));
   }
   return list;
 }
 
-list_t *filter_list(list_t *list, bool(*filter) (list_value_t value)) {
+list_t *filter_list(list_t *list, bool(*filter) (list_element_t value)) {
   list_t *l = realloc_list(NULL, list->length);
   if (l != NULL) {
     l->length = 0;
     for(size_t i = 0; i < list->length; i++) {
-      if (filter(list->values[i])) {
-        l->values[l->length++] = list->values[i];
+      if (filter(list->elements[i])) {
+        l->elements[l->length++] = list->elements[i];
       }
     }
+    if (l->length < list->length * REALLOC_FRACTION) {
+      l = realloc_list(l, l->length);
+    }
   }
-  if (l->length < list->length * REALLOC_FRACTION) {
-    l = realloc_list(l, l->length);
-  }
+
   return l;
 }
 
@@ -46,33 +50,33 @@ size_t length_list(list_t *list) {
   return list->length;
 }
 
-list_t *map_list(list_t * list, list_value_t(*map) (list_value_t value)) {
+list_t *map_list(list_t * list, list_element_t(*map) (list_element_t value)) {
   list_t *l = realloc_list(NULL, list->length);
   if (l != NULL) {
     l->length = list->length;
     for(size_t i = 0; i < list->length; i++) {
-      l->values[i] = map(list->values[i]);
+      l->elements[i] = map(list->elements[i]);
     }
   }
   return l;
 }
 
-list_value_t foldl_list(list_t * list, list_value_t initial,
-                        list_value_t(*foldl) (list_value_t value,
-                                              list_value_t initial)) {
-  list_value_t acc = initial;
+list_element_t foldl_list(list_t * list, list_element_t initial,
+                        list_element_t(*foldl) (list_element_t value,
+                                              list_element_t initial)) {
+  list_element_t acc = initial;
   for (size_t i = 0; i < list->length; i++) {
-    acc = foldl(list->values[i], acc);
+    acc = foldl(list->elements[i], acc);
   }
   return acc;
 }
 
-list_value_t foldr_list(list_t * list, list_value_t initial,
-                        list_value_t(*foldr) (list_value_t value,
-                                              list_value_t initial)) {
-  list_value_t acc = initial;
+list_element_t foldr_list(list_t * list, list_element_t initial,
+                        list_element_t(*foldr) (list_element_t value,
+                                              list_element_t initial)) {
+  list_element_t acc = initial;
   for (size_t i = list->length; i > 0; i--) {
-    acc = foldr(list->values[i - 1], acc);
+    acc = foldr(list->elements[i - 1], acc);
   }
   return acc;
 }
@@ -82,7 +86,7 @@ list_t *reverse_list(list_t *list) {
   if (reversed != NULL) {
     reversed->length = 0;
     for(size_t i = list->length; i > 0; i--) {
-      reversed->values[reversed->length++] = list->values[i - 1];
+      reversed->elements[reversed->length++] = list->elements[i - 1];
     }
   }
   return reversed;
@@ -95,5 +99,5 @@ void delete_list(list_t * list) {
 }
 
 static inline list_t *realloc_list(list_t *list, size_t length) {
-  return (list_t *)realloc(list, sizeof(list_t) + length * sizeof(list_value_t));
+  return (list_t *)realloc(list, sizeof(list_t) + length * sizeof(list_element_t));
 }
