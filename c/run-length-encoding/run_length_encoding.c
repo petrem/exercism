cb563905d75fe02d8dcfedabefe414aa613163d9
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-19 19:43:33 +0200

    exercism:c:run-length-encoding

--- c/run-length-encoding/run_length_encoding.c
+++ c/run-length-encoding/run_length_encoding.c
@@ -1,4 +1,6 @@
-// with RUBBISH memory handling
+// Pre-parse the inputs and compute required output size
+// I wonder how this would compare with a realloc-based approach, or perhaps
+//
 
 #include "run_length_encoding.h"
 
@@ -7,14 +9,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+static unsigned int n_digits(unsigned int n);
+
+
 char *encode(const char *text) {
   if (text == NULL)
     return NULL;
-  char *encoded = calloc(100, 1);
-  const char *cursor_in = text;
-  char *cursor_out = encoded, current;
-  unsigned int repeat = 0;
 
+  const char *cursor_in = text;
+  char current;
+  unsigned int repeat;
+  unsigned int length = 0;
+
+  // pre-calculate required space
+
+  while ((current = *cursor_in)) {
+    for(repeat = 0; *cursor_in == current; repeat++, cursor_in++);
+    length += 1 + n_digits(repeat);
+  }
+  
+  char *encoded = calloc(length + 1, 1);
+  char *cursor_out = encoded;
+  cursor_in = text;
   while ((current=*cursor_in)) {
     for(repeat = 0; *cursor_in == current; repeat++, cursor_in++);
     if (repeat > 1) {
@@ -29,8 +46,22 @@ char *encode(const char *text) {
 char *decode(const char *data) {
   if (data == NULL)
     return NULL;
-  char *decoded = calloc(100, 1);
-  const char *cursor_in = data;
+
+  char *cursor_in = (char *)data;
+  unsigned int length = 0;
+  while(*cursor_in) {
+    unsigned int repeat;
+    int read_size;
+    if (!isspace(*cursor_in) && sscanf(cursor_in, "%u%n", &repeat, &read_size) == 1) {
+      cursor_in += read_size;
+    } else {
+      repeat = 1;
+    }
+    cursor_in += 1;
+    length += repeat;
+  }
+  char *decoded = calloc(length + 1, 1);
+  cursor_in = (char *)data;
   char *cursor_out = decoded;
   while(*cursor_in) {
     unsigned int repeat;
@@ -47,4 +78,17 @@ char *decode(const char *data) {
     }
   }
   return decoded;
+}
+
+static unsigned int n_digits(unsigned int n) {
+  return (n < 10 ? 1 :   
+        (n < 100 ? 2 :   
+        (n < 1000 ? 3 :   
+        (n < 10000 ? 4 :   
+        (n < 100000 ? 5 :   
+        (n < 1000000 ? 6 :   
+        (n < 10000000 ? 7 :  
+        (n < 100000000 ? 8 :  
+        (n < 1000000000 ? 9 :  
+        10)))))))));
 }
