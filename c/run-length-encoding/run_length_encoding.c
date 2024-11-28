e77c9ab8ecf8877faefd306a0cfc838ad631123d
Petre Mierlutiu <petrem@users.noreply.github.com>
2024-11-28 11:08:56 +0200

    exercism:c:run-length-encoding

--- c/run-length-encoding/run_length_encoding.c
+++ c/run-length-encoding/run_length_encoding.c
@@ -1,69 +1,60 @@
-// Pre-parse the inputs and compute required output size
-// I wonder how this would compare with a realloc-based approach, or perhaps
-//
-
 #include "run_length_encoding.h"
 
 #include <ctype.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
-static unsigned int n_digits(unsigned int n);
-
+#include <string.h>
 
 char *encode(const char *text) {
   if (text == NULL)
     return NULL;
 
+  // on advice of counsel, instead of pre-computing the necessary space,
+  // I'll re-allocate as needed
+  size_t length = strlen(text);
+  char *encoded = calloc(length + 1, 1);
+  char *cursor_out = encoded;
   const char *cursor_in = text;
   char current;
   unsigned int repeat;
-  unsigned int length = 0;
-
-  // pre-calculate required space
+  size_t repeat_size;
 
   while ((current = *cursor_in)) {
     for(repeat = 0; *cursor_in == current; repeat++, cursor_in++);
-    length += 1 + n_digits(repeat);
-  }
-  
-  char *encoded = calloc(length + 1, 1);
-  char *cursor_out = encoded;
-  cursor_in = text;
-  while ((current=*cursor_in)) {
-    for(repeat = 0; *cursor_in == current; repeat++, cursor_in++);
     if (repeat > 1) {
-      cursor_out += sprintf(cursor_out, "%u", repeat);
+      repeat_size = sprintf(cursor_out, "%u", repeat);
+      if (repeat_size >= length)
+        goto error;
+      cursor_out += repeat_size;
+      length -= repeat_size;
     }
+    if (length == 0)
+      goto error;
     *cursor_out++ = current;
+    length--;
   }
-  return encoded;
+    
+  return realloc(encoded, cursor_out - encoded + 1);
+ error:
+  free(encoded);
+  return NULL;
+  
 }
 
+// This is set small so that possible bugs are more likely to crop up
+#define DECODE_BUF_LEN 20
 
 char *decode(const char *data) {
   if (data == NULL)
     return NULL;
 
   char *cursor_in = (char *)data;
-  unsigned int length = 0;
-  while(*cursor_in) {
-    unsigned int repeat;
-    int read_size;
-    if (!isspace(*cursor_in) && sscanf(cursor_in, "%u%n", &repeat, &read_size) == 1) {
-      cursor_in += read_size;
-    } else {
-      repeat = 1;
-    }
-    cursor_in += 1;
-    length += repeat;
-  }
-  char *decoded = calloc(length + 1, 1);
-  cursor_in = (char *)data;
-  char *cursor_out = decoded;
-  while(*cursor_in) {
+  size_t length = DECODE_BUF_LEN;
+  char *decoded = malloc(length + 1);
+  size_t offset = 0;
+  while (*cursor_in) {
     unsigned int repeat;
     int read_size;
     char current;
@@ -73,22 +64,22 @@ char *decode(const char *data) {
       repeat = 1;
     }
     current = *cursor_in++;
+    if (length < repeat + 1) {
+      // This is not efficient, when DECODE_BUF_LEN is small compared to the decoded text.
+      // But I'll keep it simple.
+      length = DECODE_BUF_LEN * ((repeat / DECODE_BUF_LEN) + 1);
+      char *new_decoded = realloc(decoded, offset + length + 1);
+      if (new_decoded == NULL) {
+        free(decoded);
+        return NULL;
+      }
+      decoded = new_decoded;
+    }
     for (; repeat >= 1; repeat--) {
-      *cursor_out++ = current;
+      decoded[offset++] = current;
+      length -= 1;
     }
   }
-  return decoded;
-}
-
-static unsigned int n_digits(unsigned int n) {
-  return (n < 10 ? 1 :   
-        (n < 100 ? 2 :   
-        (n < 1000 ? 3 :   
-        (n < 10000 ? 4 :   
-        (n < 100000 ? 5 :   
-        (n < 1000000 ? 6 :   
-        (n < 10000000 ? 7 :  
-        (n < 100000000 ? 8 :  
-        (n < 1000000000 ? 9 :  
-        10)))))))));
+  decoded[offset] = '\0';
+  return realloc(decoded, offset + 1);
 }
