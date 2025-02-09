a521f85b3666cc09ccdf9fbafcf026c9c0f677ee
Petre Mierlutiu <petrem@users.noreply.github.com>
2025-02-09 17:01:41 +0200

    exercism:haskell:word-count

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,31 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module WordCount (wordCount) where
 
-import Data.Char (isAlphaNum, toLower)
+import Data.Char (isAlphaNum)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import Data.Text (Text)
+import qualified Data.Text as T
 
-wordCount :: String -> Map String Int
-wordCount = countWords . words' . fmap toLower
+wordCount :: Text -> Map Text Int
+wordCount = countWords . words' . T.toLower
 
-countWords :: [String] -> Map String Int
+countWords :: [Text] -> Map Text Int
 countWords = foldr (\s r -> M.insertWith (+) s 1 r) M.empty
 
 isSeparator :: Char -> Bool
 isSeparator = not . ((||) <$> Data.Char.isAlphaNum <*> (== '\''))
 
-words' :: String -> [String]
-words' s = case dropWhile isSeparator s of
+words' :: Text -> [Text]
+words' s = case T.dropWhile isSeparator s of
   "" -> []
   s' -> peelApostrophes w : words' s''
     where
-      (w, s'') = break isSeparator s'
+      (w, s'') = T.break isSeparator s'
 
-peelApostrophes :: String -> String
-peelApostrophes = dropWhile (== '\'') . foldr go ""
-  where
-    go '\'' [] = []
-    go c acc = c : acc
+peelApostrophes :: Text -> Text
+peelApostrophes = T.dropAround (== '\'')
 
 -- TODO:
--- - use Data.Text
 -- - look into Multiset
