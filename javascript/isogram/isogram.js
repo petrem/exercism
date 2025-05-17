adf8d7311a208dd3474f1ddae729a040e6e31cbb
Petre Mierlutiu <petrem@users.noreply.github.com>
2025-05-17 17:16:22 +0300

    exercism:javascript:isogram

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,2 +1,10 @@
-// eslint-disable-next-line no-extra-boolean-cast
-export const isIsogram = (phrase) => !Boolean(phrase.match(/([a-z]).*\1/i));
+// // eslint-disable-next-line no-extra-boolean-cast
+// export const isIsogram = (phrase) => !Boolean(phrase.match(/([a-z]).*\1/i));
+
+export function isIsogram(string) {
+  return [...string
+    .replaceAll(" ", "")
+    .replaceAll("-", "")
+    .toLowerCase()]
+    .every((x, k, xs) => xs.indexOf(x, k+1) === -1);
+}
