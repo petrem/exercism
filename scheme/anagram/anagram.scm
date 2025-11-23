95793fd20863a7a4aa3bac3f7b0e2eb0c287928e
Petre Mierlutiu <petrem@users.noreply.github.com>
2025-11-23 19:26:22 +0200

    exercism:scheme:anagram

--- scheme/anagram/anagram.scm
+++ scheme/anagram/anagram.scm
@@ -3,7 +3,8 @@
 (define (anagram target words)
   (let ((target-letters (sort (string-downcase target) char<?)))
     (filter (lambda (word)
-              (and (not (string-ci=? target word))
-                   (string=? target-letters (sort (string-downcase word) char<?))))
+              (and (string=? target-letters (sort (string-downcase word) char<?))
+                   (not (string-ci=? target word))))
+                   
             words)))
     
