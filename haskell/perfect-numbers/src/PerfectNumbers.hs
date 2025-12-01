0c8f0fda0cf75900653213c275bda986b27130ff
Petre Mierlutiu <petrem@users.noreply.github.com>
2025-12-01 13:05:23 +0200

    exercism:haskell:perfect-numbers

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -1,18 +1,8 @@
-module PerfectNumbers (classify, Classification(..)) where
+module PerfectNumbers (classify, Classification (..)) where
 
-data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
-
-
-properDivisors :: Int -> [Int]
-properDivisors x = [y | y <- [1..x-2], x `mod` y == 0]
-
-aliquotSum :: Int -> Int
-aliquotSum = sum . properDivisors
+data Classification = Deficient | Perfect | Abundant deriving (Enum, Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify x
-  | x <= 0 = Nothing
-  | otherwise = Just $ case compare <$> aliquotSum <*> id $ x of
-                  LT -> Deficient
-                  EQ -> Perfect
-                  GT -> Abundant
+classify x = toEnum . fromEnum . compare (sum properDivisors) <$> if x <= 0 then Nothing else Just x
+  where
+    properDivisors = [y | y <- [1 .. x - 2], x `mod` y == 0]
