df4df5696348b091eb5c9d8497984c7bf73f74cf
Petre Mierlutiu <petrem@users.noreply.github.com>
2025-12-24 19:51:09 +0200

    exercism:python:wordy

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,23 +1,48 @@
-import operator as op
-import re
+"""A first, simple version."""
 
-
-OPERATIONS = {
-    "plus": op.add,
-    "minus": op.sub,
-    "multiplied by": op.mul,
-    "divided by": op.truediv,
+OPERATORS = {
+    "plus": lambda lhs, rhs: lhs + rhs,
+    "minus": lambda lhs, rhs: lhs - rhs,
+    "divided": lambda lhs, rhs: lhs / rhs,
+    "multiplied": lambda lhs, rhs: lhs * rhs,
 }
-WORDY_RE = re.compile(
-    r"What is\s+(?:(?P<number>\d+)|"
-    rf"(?P<expr>\d+\s+[\d\w ]+\s+\d+))\s*\?",
-)
 
 
 def answer(question):
-    if m := WORDY_RE.fullmatch(question):
-        if number := m.group("number"):
-            return int(number)
-        if expr := m.group("expr"):
-            ...
-    raise ValueError()
+    expression_string = (
+        question.removeprefix("What is").strip().removesuffix("?").strip()
+    )
+    expression = [word for word in expression_string.split(" ") if word != "by"]
+
+    if len(expression) == 0:
+        raise ValueError("syntax error")
+
+    while len(expression) > 1:
+        print(f"{expression=}")
+        try:
+            lhs, op, rhs, *rest = expression
+        except ValueError as e:
+            _lhs, op, *_rest = expression
+            try:
+                int(op)
+            except ValueError:
+                if op not in OPERATORS:
+                    raise ValueError("unknown operation") from e
+            raise ValueError("syntax error") from e
+
+        try:
+            lhs = int(lhs)
+            rhs = int(rhs)
+        except ValueError as e:
+            raise ValueError("syntax error") from e
+
+        try:
+            op_fn = OPERATORS[op]
+        except KeyError as e:
+            raise ValueError("syntax error") from e
+
+        expression[:] = [op_fn(lhs, rhs), *rest]
+    try:
+        return int(expression[0])
+    except ValueError as e:
+        raise ValueError("syntax error") from e
