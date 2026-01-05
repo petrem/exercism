f96e6329d5deb0fdbbb6ea0b758cb36610b21df6
Petre Mierlutiu <petrem@users.noreply.github.com>
2026-01-05 15:10:21 +0200

    exercism:scheme:rotational-cipher

--- scheme/rotational-cipher/rotational-cipher.scm
+++ scheme/rotational-cipher/rotational-cipher.scm
@@ -6,8 +6,8 @@
   (define (rotate-char c lookup)
     (string-ref lookup (+ dx (- (char->integer c) (char->integer (string-ref lookup 0))))))
   (string-map (lambda (c)
-                (cond ((char-set-contains? char-set:upper-case c) (rotate-char c repeated-uppercase))
-                      ((char-set-contains? char-set:lower-case c) (rotate-char c repeated-lowercase))
+                (cond ((char-upper-case? c) (rotate-char c repeated-uppercase))
+                      ((char-lower-case? c) (rotate-char c repeated-lowercase))
                       (else c)))
                 phrase))
 
