b1051954e324b56771e489b5a4b5498c3316150c
Petre Mierlutiu <petrem@users.noreply.github.com>
2026-01-06 15:46:07 +0200

    exercism:scheme:bob

--- scheme/bob/bob.scm
+++ scheme/bob/bob.scm
@@ -1,6 +1,7 @@
 (import (rnrs))
 
 (define (response-for message)
+
   (define (question? s)
     (string-suffix? "?" s))
 
@@ -14,9 +15,19 @@
   (define (silence? s)
     (string-null? s))
 
-  (let ((trimmed-message (string-trim-both message)))
-    (cond ((silence? trimmed-message) "Fine. Be that way!")
-          ((yell-question? trimmed-message) "Calm down, I know what I'm doing!")
-          ((question? trimmed-message) "Sure.")
-          ((yell? trimmed-message) "Whoa, chill out!")
-          (else "Whatever."))))
+  (define (classify)
+    (let ((trimmed-message (string-trim-both message)))
+     (cond ((silence? trimmed-message) 'Silence)
+          ((yell-question? trimmed-message) 'Yell-Question)
+          ((question? trimmed-message) 'Question)
+          ((yell? trimmed-message) 'Yell)
+          (else 'Other))))
+
+  (define lackadaisical-answers
+    '((Silence . "Fine. Be that way!")
+      (Yell-Question . "Calm down, I know what I'm doing!")
+      (Question . "Sure.")
+      (Yell . "Whoa, chill out!")
+      (Other . "Whatever.")))
+
+  (assq-ref lackadaisical-answers (classify)))
