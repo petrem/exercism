1532e240cfb0f79c209448eaa6135024497e8865
Petre Mierlutiu <petrem@users.noreply.github.com>
2026-01-11 16:24:40 +0200

    exercism:scheme:hamming

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -1,4 +1,4 @@
 (import (rnrs))
 
 (define (hamming-distance strand-a strand-b)
-  (length (filter not (map eq? (string->list strand-a) (string->list strand-b)))))
+  (length (filter not (map eqv? (string->list strand-a) (string->list strand-b)))))
