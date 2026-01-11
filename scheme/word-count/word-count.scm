dfac7eeb617198b02cda8dc6a8f5c5f7a9ffa9b8
Petre Mierlutiu <petrem@users.noreply.github.com>
2026-01-11 16:55:52 +0200

    exercism:scheme:word-count

--- scheme/word-count/word-count.scm
+++ scheme/word-count/word-count.scm
@@ -1,24 +1,24 @@
 (import (rnrs))
 
 (define (word-count sentence)
-  (define (increment-word-count word counts)
-    (let ((current-count (assoc-ref counts word)))
-      (assoc-set! counts word (1+ (or current-count 0)))))
+  (let ((increment-word-count
+         (lambda (word counts)
+           (let ((current-count (assoc-ref counts word)))
+             (assoc-set! counts word (1+ (or current-count 0))))))
+        (split-words
+         (lambda (text)
+           (let* ((token-set (char-set-adjoin char-set:letter+digit #\'))
+                  (words-with-apostrophes (string-tokenize text token-set)))
+             (map (lambda (word)
+                    (if (and (string-prefix? "'" word)
+                             (string-suffix? "'" word))
+                        (string-trim-both word #\')
+                        word))
+                  words-with-apostrophes)))))
 
-  (define (split-words sentence)
-    (let* ((token-set (char-set-adjoin char-set:letter+digit #\'))
-           (words-with-apostrophes (string-tokenize sentence token-set)))
-      (map (lambda (word)
-             (if (and (string-prefix? "'" word)
-                      (string-suffix? "'" word))
-                 (string-trim-both word #\')
-                 word))
-           words-with-apostrophes)))
-           
-
-  (let loop ((words (split-words (string-downcase sentence)))
-             (counts '()))
-    (if (null? words)
-        counts
-        (loop (cdr words) (increment-word-count (car words) counts)))))
+    (let loop ((words (split-words (string-downcase sentence)))
+               (counts '()))
+      (if (null? words)
+          counts
+          (loop (cdr words) (increment-word-count (car words) counts))))))
 
