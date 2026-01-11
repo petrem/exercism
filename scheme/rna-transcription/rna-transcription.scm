9cc68c045d615eb7c269b95d02c4c9078f2a8772
Petre Mierlutiu <petrem@users.noreply.github.com>
2026-01-11 16:22:43 +0200

    exercism:scheme:rna-transcription

--- scheme/rna-transcription/rna-transcription.scm
+++ scheme/rna-transcription/rna-transcription.scm
@@ -1,10 +1,11 @@
 (import (rnrs))
 
 (define (dna->rna dna)
-  (define (dna-nucleotide->rna-nucleotide c)
-    (case c
-      ((#\A) #\U)
-      ((#\C) #\G)
-      ((#\G) #\C)
-      ((#\T) #\A)))
-  (string-map dna-nucleotide->rna-nucleotide dna))
+  (let ((dna-nucleotide->rna-nucleotide
+         (lambda (c)
+           (case c
+             ((#\A) #\U)
+             ((#\C) #\G)
+             ((#\G) #\C)
+             ((#\T) #\A)))))
+    (string-map dna-nucleotide->rna-nucleotide dna)))
